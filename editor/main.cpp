afc47cfe3fd975ab75e70f1164f6ad89f7dfb2d4
iMeaNz <mehdy.morvan@epitech.eu>
2025-02-24 13:47:21 +0900

    fix(materials): now pushing first the main scene onto the window stack to avoid the resizing that was bugging the camera when first clicking

--- editor/main.cpp
+++ editor/main.cpp
@@ -30,10 +30,12 @@ int main(int argc, char **argv)
         nexo::editor::Editor editor;
         const auto &sceneViewManager = nexo::editor::SceneViewManager::getInstance();
         sceneViewManager->addNewScene("Default scene", std::make_shared<nexo::editor::MainScene>("Default scene", true));
-        editor.registerWindow("Scene Tree", std::make_shared<nexo::editor::SceneTreeWindow>());
         editor.registerWindow("Scene view manager", sceneViewManager);
+
+        editor.registerWindow("Scene Tree", std::make_shared<nexo::editor::SceneTreeWindow>());
         editor.registerWindow("Inspector", std::make_shared<nexo::editor::InspectorWindow>());
         editor.registerWindow("Console", std::make_shared<nexo::editor::ConsoleWindow>(editor));
+
         editor.init();
 
         while (editor.isOpen())
