6330533cb0966aa6e5014d10ad769855511d8c75
iMeaNz <mehdy.morvan@epitech.eu>
2025-03-17 17:34:34 +0900

    refactor(materials): removed unused utils function

--- editor/src/DocumentWindows/EntityProperties/TransformProperty.cpp
+++ editor/src/DocumentWindows/EntityProperties/TransformProperty.cpp
@@ -29,45 +29,6 @@ namespace nexo::editor {
 
     }
 
-    static void drawRowWithDragFloat(const char *label, float *values)
-    {
-	    std::string labelStr = label;
-	    std::string labelX = std::string("##X") + labelStr;
-	    std::string labelY = std::string("##Y") + labelStr;
-	    std::string labelZ = std::string("##Z") + labelStr;
-
-	    std::string badgeLabelX = std::string("X##") + labelStr;
-	    std::string badgeLabelY = std::string("Y##") + labelStr;
-	    std::string badgeLabelZ = std::string("Z##") + labelStr;
-
-	    ImGui::TableNextRow();
-
-		ChannelLabel chanLabel;
-		chanLabel.label = std::string(label);
-		chanLabel.fixedWidth = -1.0f;
-
-		float badgeSize = ImGui::GetFrameHeight();
-		std::vector<Badge> badges;
-		badges.reserve(3);
-		badges.push_back(Badge(badgeLabelX, ImVec2(badgeSize, badgeSize), IM_COL32(80, 0, 0, 255), IM_COL32(80, 0, 0, 255), IM_COL32(80, 0, 0, 255), IM_COL32(255, 180, 180, 255)));
-		badges.push_back(Badge(badgeLabelY, ImVec2(badgeSize, badgeSize), IM_COL32(0, 80, 0, 255), IM_COL32(0, 80, 0, 255), IM_COL32(0, 80, 0, 255), IM_COL32(180, 255, 180, 255)));
-		badges.push_back(Badge(badgeLabelZ, ImVec2(badgeSize, badgeSize), IM_COL32(0, 0, 80, 255), IM_COL32(0, 0, 80, 255), IM_COL32(0, 0, 80, 255), IM_COL32(180, 180, 255, 255)));
-
-		std::vector<DragFloat> sliders;
-		sliders.reserve(3);
-		sliders.push_back(DragFloat(labelX, &values[0], 0.3f, -FLT_MAX, FLT_MAX, IM_COL32(60, 60, 60, 255), IM_COL32(80, 80, 80, 255), IM_COL32(100, 100, 100, 255), "%.2f"));
-		sliders.push_back(DragFloat(labelY, &values[1], 0.3f, -FLT_MAX, FLT_MAX, IM_COL32(60, 60, 60, 255), IM_COL32(80, 80, 80, 255), IM_COL32(100, 100, 100, 255), "%.2f"));
-		sliders.push_back(DragFloat(labelZ, &values[2], 0.3f, -FLT_MAX, FLT_MAX, IM_COL32(60, 60, 60, 255), IM_COL32(80, 80, 80, 255), IM_COL32(100, 100, 100, 255), "%.2f"));
-
-		Channels channels;
-		channels.count = 3;
-		channels.badges = badges;
-		channels.sliders = sliders;
-
-		EntityPropertiesComponents::drawRowLabel(chanLabel);
-		EntityPropertiesComponents::drawRowDragFloat(channels);
-    }
-
     int TransformProperty::show(ecs::Entity entity)
     {
         auto const& App = getApp();
