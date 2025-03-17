a0b838b73b61278b9c263f6aaf4d2a5374de11e4
iMeaNz <mehdy.morvan@epitech.eu>
2025-03-17 23:12:23 +0900

    tests(materials): added tests for the light utils functions

--- tests/utils/comparison.hpp
+++ tests/utils/comparison.hpp
@@ -47,9 +47,16 @@ namespace nexo::utils {
         EXPECT_NEAR(a.x, b.x, epsilon);
         EXPECT_NEAR(a.y, b.y, epsilon);
     }
+
+    inline void expectFloatNear(const float a, const float b, const float epsilon = 1e-5f)
+    {
+        EXPECT_NEAR(a, b, epsilon);
+    }
+
 }
 
 #define EXPECT_MAT4_NEAR(a, b, epsilon) nexo::utils::expectMat4Near(a, b, epsilon)
 #define EXPECT_VEC4_NEAR(a, b, epsilon) nexo::utils::expectVec4Near(a, b, epsilon)
 #define EXPECT_VEC2_NEAR(a, b, epsilon) nexo::utils::expectVec2Near(a, b, epsilon)
 #define EXPECT_VEC3_NEAR(a, b, epsilon) nexo::utils::expectVec3Near(a, b, epsilon)
+#define EXPECT_FLOAT_NEAR(a, b, epsilon) nexo::utils::expectFloatNear(a, b, epsilon)
