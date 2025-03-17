5ecd89b47a0b4290c99e2fff6ae4e72ad65c198c
iMeaNz <mehdy.morvan@epitech.eu>
2025-03-17 17:30:26 +0900

    feat(materials): added points lights properties

---
+++ editor/src/DocumentWindows/EntityProperties/PointLightProperty.hpp
@@ -0,0 +1,23 @@
+//// PointLightProperty.hpp ///////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Mehdy MORVAN
+//  Date:        17/03/2025
+//  Description: Header file for the point light property
+//
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "AEntityProperty.hpp"
+
+namespace nexo::editor {
+	class PointLightProperty : public AEntityProperty {
+		public:
+			static int show(ecs::Entity entity);
+	};
+}
