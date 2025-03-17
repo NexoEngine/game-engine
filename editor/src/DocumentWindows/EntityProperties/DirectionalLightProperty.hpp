043955c14b5323ace2a3648b921187a363b4c92f
iMeaNz <mehdy.morvan@epitech.eu>
2025-03-17 17:30:44 +0900

    feat(materials): added directional lights inspector

---
+++ editor/src/DocumentWindows/EntityProperties/DirectionalLightProperty.hpp
@@ -0,0 +1,23 @@
+//// DirectionalLightProperty.hpp ///////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Mehdy MORVAN
+//  Date:        17/03/2025
+//  Description: Header file for the directional light property
+//
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "AEntityProperty.hpp"
+
+namespace nexo::editor {
+	class DirectionalLightProperty : public AEntityProperty {
+		public:
+			static int show(ecs::Entity entity);
+	};
+}
