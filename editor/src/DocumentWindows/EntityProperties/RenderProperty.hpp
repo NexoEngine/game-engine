13d07d5cc017e4bdf59ed8d9fab52bf172757674
iMeaNz <mehdy.morvan@epitech.eu>
2025-03-20 10:10:25 +0900

    feat(materials): implement the new material popup

--- editor/src/DocumentWindows/EntityProperties/RenderProperty.hpp
+++ editor/src/DocumentWindows/EntityProperties/RenderProperty.hpp
@@ -8,22 +8,22 @@
 #pragma once
 
 #include "AEntityProperty.hpp"
+#include "DocumentWindows/PopupManager.hpp"
 
 namespace nexo::editor {
     class RenderProperty : public nexo::editor::AEntityProperty {
         public:
-            // using ComponentType = ecs::components::render::render_t;
-
             explicit RenderProperty(const std::string& name);
             ~RenderProperty() override;
 
             void update() final;
 
             static int show(ecs::Entity entity);
+            static void createMaterialPopup(ecs::Entity entity);
             void showEnd() final;
 
             static bool showMaterialInspector;
             static components::Material *selectedMaterial;
-
+            static PopupManager popupManager;
     };
 }
