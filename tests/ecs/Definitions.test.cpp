76d111628532711ca6461a33a5c2d3302688a0b4
iMeaNz <mehdy.morvan@epitech.eu>
2025-04-09 21:09:45 +0900

    tests(ecs-opti): add tests for the new features of the ecs

---
+++ tests/ecs/Definitions.test.cpp
@@ -0,0 +1,89 @@
+//// Definitions.test.cpp ///////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Mehdy MORVAN
+//  Date:        09/04/2025
+//  Description: Test file for the utils in the definitions header
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <gtest/gtest.h>
+#include "Definitions.hpp"
+#include <string>
+#include <vector>
+#include <type_traits>
+
+namespace nexo::ecs {
+
+	class DefinitionsTest : public ::testing::Test {};
+
+	// Test structures for component type ID assignment
+	struct TestComponent1 {};
+	struct TestComponent2 {};
+	struct TestComponent3 {};
+
+	// A structure template to test type uniqueness
+	template<int N>
+	struct GenericComponent {};
+
+	// getUniqueComponentTypeID function tests
+	TEST_F(DefinitionsTest, GetUniqueComponentTypeIDAssignsUniqueIDs) {
+	    // Get IDs for multiple component types
+	    ComponentType id1 = getUniqueComponentTypeID<TestComponent1>();
+	    ComponentType id2 = getUniqueComponentTypeID<TestComponent2>();
+	    ComponentType id3 = getUniqueComponentTypeID<TestComponent3>();
+
+	    // IDs should be unique
+	    EXPECT_NE(id1, id2);
+	    EXPECT_NE(id1, id3);
+	    EXPECT_NE(id2, id3);
+
+	    // IDs should be assigned sequentially starting from 0
+	    EXPECT_EQ(id2, id1 + 1);
+	    EXPECT_EQ(id3, id2 + 1);
+	}
+
+	TEST_F(DefinitionsTest, GetUniqueComponentTypeIDReturnsSameIDForSameType) {
+	    // Get the ID for the same type multiple times
+	    ComponentType id1 = getUniqueComponentTypeID<TestComponent1>();
+	    ComponentType id2 = getUniqueComponentTypeID<TestComponent1>();
+	    ComponentType id3 = getUniqueComponentTypeID<TestComponent1>();
+
+	    // Same type should get the same ID
+	    EXPECT_EQ(id1, id2);
+	    EXPECT_EQ(id1, id3);
+	}
+
+	// getComponentTypeID function tests
+	TEST_F(DefinitionsTest, GetComponentTypeIDRemovesTypeQualifiers) {
+	    // Get IDs for TestComponent1 with different qualifiers
+	    ComponentType baseId = getComponentTypeID<TestComponent1>();
+	    ComponentType constId = getComponentTypeID<const TestComponent1>();
+	    ComponentType volatileId = getComponentTypeID<volatile TestComponent1>();
+	    ComponentType refId = getComponentTypeID<TestComponent1&>();
+	    ComponentType constRefId = getComponentTypeID<const TestComponent1&>();
+
+	    // All should return the same ID regardless of qualifiers
+	    EXPECT_EQ(baseId, constId);
+	    EXPECT_EQ(baseId, volatileId);
+	    EXPECT_EQ(baseId, refId);
+	    EXPECT_EQ(baseId, constRefId);
+	}
+
+	TEST_F(DefinitionsTest, GetComponentTypeIDForTemplatedTypes) {
+	    // Test with template types
+	    ComponentType id1 = getComponentTypeID<GenericComponent<1>>();
+	    ComponentType id2 = getComponentTypeID<GenericComponent<2>>();
+	    ComponentType id3 = getComponentTypeID<GenericComponent<3>>();
+
+	    // Different template instantiations should get different IDs
+	    EXPECT_NE(id1, id2);
+	    EXPECT_NE(id1, id3);
+	    EXPECT_NE(id2, id3);
+	}
+}
--- tests/ecs/Entity.test.cpp
+++ tests/ecs/Entity.test.cpp
@@ -1,4 +1,4 @@
-//// Entity.test.cpp //////////////////////////////////////////////////////////
+//// Entity.test.cpp ///////////////////////////////////////////////////
 //
 //  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
 //  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
@@ -6,246 +6,284 @@
 //  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
 //  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
 //
-//  Author:      Mehdy MORVAN
-//  Date:        26/11/2024
-//  Description: Test file for the entity manager
+//  Author:      iMeaNz
+//  Date:        2025-04-09
+//  Description: Test file for Entity Manager class
 //
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include "ecs/Entity.hpp"
-#include "Components.hpp"
+#include "Entity.hpp"
+#include "ECSExceptions.hpp"
+#include <vector>
+#include <set>
+#include <algorithm>
 
 namespace nexo::ecs {
-    class EntityManagerTest : public ::testing::Test {
-        protected:
-        void SetUp() override {
-            entityManager = std::make_unique<EntityManager>();
-        }
 
-        std::unique_ptr<EntityManager> entityManager;
-    };
+	class EntityManagerTest : public ::testing::Test {
+	protected:
+	    EntityManager entityManager;
 
-    TEST_F(EntityManagerTest, CreateAndDestroyEntity) {
-        Entity entity = entityManager->createEntity();
-        EXPECT_EQ(entity, 0); // First entity should have ID 0
+	    // Helper to create multiple entities
+	    std::vector<Entity> createMultipleEntities(size_t count) {
+	        std::vector<Entity> entities;
+	        entities.reserve(count);
+	        for (size_t i = 0; i < count; ++i) {
+	            entities.push_back(entityManager.createEntity());
+	        }
+	        return entities;
+	    }
+	};
 
-        entityManager->destroyEntity(entity);
+	// Test constructor
+	TEST_F(EntityManagerTest, ConstructorInitializesCorrectly) {
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 0);
 
-        // Recreate the entity, it should use the ID in front
-        Entity reusedEntity = entityManager->createEntity();
-        EXPECT_EQ(reusedEntity, 0);
-    }
+	    // Create and then destroy an entity to verify the pool works
+	    Entity e = entityManager.createEntity();
+	    EXPECT_EQ(e, 0); // First entity should be 0
+	    entityManager.destroyEntity(e);
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 0);
 
-    TEST_F(EntityManagerTest, TooManyEntities) {
-        for (Entity i = 0; i < MAX_ENTITIES; ++i) {
-            EXPECT_NO_THROW(entityManager->createEntity());
-        }
+	    // Creating again should reuse the entity ID
+	    Entity e2 = entityManager.createEntity();
+	    EXPECT_EQ(e2, 0);
+	}
 
-        EXPECT_THROW(entityManager->createEntity(), TooManyEntities);
-    }
+	TEST_F(EntityManagerTest, CreateEntityReturnsUniqueIDs) {
+	    const size_t numEntities = 100;
+	    std::set<Entity> uniqueEntities;
 
-    TEST_F(EntityManagerTest, SetAndGetSignature) {
-        Entity entity = entityManager->createEntity();
-        Signature signature;
-        signature.set(1); // Set the 1st bit
+	    for (size_t i = 0; i < numEntities; ++i) {
+	        Entity e = entityManager.createEntity();
+	        EXPECT_EQ(uniqueEntities.count(e), 0) << "Entity ID " << e << " was issued multiple times";
+	        uniqueEntities.insert(e);
+	    }
 
-        entityManager->setSignature(entity, signature);
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), numEntities);
+	}
 
-        Signature retrieved = entityManager->getSignature(entity);
-        EXPECT_EQ(retrieved, signature);
-    }
+	// Test destroyEntity
+	TEST_F(EntityManagerTest, DestroyEntityRemovesFromLivingEntities) {
+	    // Create some entities
+	    auto entities = createMultipleEntities(5);
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 5);
 
-    TEST_F(EntityManagerTest, SetSignatureOutOfRange) {
-        Entity invalidEntity = MAX_ENTITIES; // Invalid entity ID
-        Signature signature;
+	    // Destroy middle entity
+	    entityManager.destroyEntity(entities[2]);
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 4);
 
-        EXPECT_THROW(entityManager->setSignature(invalidEntity, signature), OutOfRange);
-    }
+	    // Check it's removed from living entities
+	    auto livingEntities = entityManager.getLivingEntities();
+	    EXPECT_FALSE(std::find(livingEntities.begin(), livingEntities.end(), entities[2]) != livingEntities.end());
 
-    TEST_F(EntityManagerTest, GetSignatureOutOfRange) {
-        Entity invalidEntity = MAX_ENTITIES; // Invalid entity ID
+	    // Other entities should still be there
+	    EXPECT_TRUE(std::find(livingEntities.begin(), livingEntities.end(), entities[0]) != livingEntities.end());
+	    EXPECT_TRUE(std::find(livingEntities.begin(), livingEntities.end(), entities[1]) != livingEntities.end());
+	    EXPECT_TRUE(std::find(livingEntities.begin(), livingEntities.end(), entities[3]) != livingEntities.end());
+	    EXPECT_TRUE(std::find(livingEntities.begin(), livingEntities.end(), entities[4]) != livingEntities.end());
+	}
 
-        EXPECT_THROW(static_cast<void>(entityManager->getSignature(invalidEntity)), OutOfRange);
-    }
+	TEST_F(EntityManagerTest, DestroyEntityThrowsWithInvalidEntity) {
+	    EXPECT_THROW({ entityManager.destroyEntity(MAX_ENTITIES); }, OutOfRange);
+	}
 
-    TEST_F(EntityManagerTest, DestroyEntityResetsSignature) {
-        Entity entity = entityManager->createEntity();
-        Signature signature;
-        signature.set(1); // Set the 1st bit
+	// Test ID recycling
+	TEST_F(EntityManagerTest, DestroyedEntityIDsAreRecycled) {
+	    // Create entities
+	    auto entities = createMultipleEntities(5);
 
-        entityManager->setSignature(entity, signature);
+	    // Destroy entities in reverse order
+	    entityManager.destroyEntity(entities[4]);
+	    entityManager.destroyEntity(entities[2]);
+	    entityManager.destroyEntity(entities[0]);
 
-        entityManager->destroyEntity(entity);
+	    // New entities should reuse IDs in LIFO order (stack behavior)
+	    Entity e1 = entityManager.createEntity();
+	    Entity e2 = entityManager.createEntity();
+	    Entity e3 = entityManager.createEntity();
 
-        Signature resetSignature = entityManager->getSignature(entity);
-        EXPECT_TRUE(resetSignature.none()); // Signature should be reset
-    }
+	    // The IDs should be reused in reverse order of destruction
+	    EXPECT_EQ(e1, entities[0]);
+	    EXPECT_EQ(e2, entities[2]);
+	    EXPECT_EQ(e3, entities[4]);
+	}
 
-    TEST_F(EntityManagerTest, DestroyEntityOutOfRange) {
-        Entity invalidEntity = MAX_ENTITIES; // Invalid entity ID
+	TEST_F(EntityManagerTest, SetAndGetSignatureWorkCorrectly) {
+	    // Create an entity
+	    Entity e = entityManager.createEntity();
 
-        EXPECT_THROW(entityManager->destroyEntity(invalidEntity), OutOfRange);
-    }
+	    // Create a test signature
+	    Signature signature;
+	    signature.set(1); // Set bit 1
+	    signature.set(3); // Set bit 3
 
-    TEST_F(EntityManagerTest, CreateAndDestroyAllEntities) {
-        std::vector<Entity> entities;
+	    // Set signature
+	    entityManager.setSignature(e, signature);
 
-        for (Entity i = 0; i < MAX_ENTITIES; ++i) {
-            entities.push_back(entityManager->createEntity());
-        }
+	    // Get signature and verify it matches
+	    Signature retrievedSignature = entityManager.getSignature(e);
+	    EXPECT_EQ(retrievedSignature, signature);
+	    EXPECT_TRUE(retrievedSignature.test(1));
+	    EXPECT_TRUE(retrievedSignature.test(3));
+	    EXPECT_FALSE(retrievedSignature.test(0));
+	    EXPECT_FALSE(retrievedSignature.test(2));
+	}
 
-        EXPECT_THROW(entityManager->createEntity(), TooManyEntities);
+	// Test setting signature for invalid entity
+	TEST_F(EntityManagerTest, SetSignatureThrowsWithInvalidEntity) {
+	    Signature signature;
+	    EXPECT_THROW({ entityManager.setSignature(MAX_ENTITIES, signature); }, OutOfRange);
+	}
 
-        for (Entity entity : entities) {
-            EXPECT_NO_THROW(entityManager->destroyEntity(entity));
-        }
+	// Test getting signature for invalid entity
+	TEST_F(EntityManagerTest, GetSignatureThrowsWithInvalidEntity) {
+	    EXPECT_THROW({ static_cast<void>(entityManager.getSignature(MAX_ENTITIES)); }, OutOfRange);
+	}
 
-        EXPECT_NO_THROW(entityManager->createEntity());
-    }
+	// Test signature is reset on entity destruction
+	TEST_F(EntityManagerTest, DestroyEntityResetsSignature) {
+	    // Create an entity and set signature
+	    Entity e = entityManager.createEntity();
 
-    TEST_F(EntityManagerTest, GetLivingEntityCount) {
-        // Initially should be 0
-        EXPECT_EQ(entityManager->getLivingEntityCount(), 0);
+	    Signature signature;
+	    signature.set(1);
+	    signature.set(3);
 
-        // Create one entity
-        Entity entity1 = entityManager->createEntity();
-        EXPECT_EQ(entityManager->getLivingEntityCount(), 1);
+	    entityManager.setSignature(e, signature);
 
-        // Create another entity
-        Entity entity2 = entityManager->createEntity();
-        EXPECT_EQ(entityManager->getLivingEntityCount(), 2);
+	    // Destroy entity
+	    entityManager.destroyEntity(e);
 
-        // Destroy one entity
-        entityManager->destroyEntity(entity1);
-        EXPECT_EQ(entityManager->getLivingEntityCount(), 1);
+	    // Create a new entity (should reuse the ID)
+	    Entity newE = entityManager.createEntity();
+	    EXPECT_EQ(newE, e);
 
-        // Destroy the other entity
-        entityManager->destroyEntity(entity2);
-        EXPECT_EQ(entityManager->getLivingEntityCount(), 0);
-    }
+	    // Signature should be reset
+	    Signature newSignature = entityManager.getSignature(newE);
+	    EXPECT_EQ(newSignature.count(), 0);
+	    EXPECT_FALSE(newSignature.test(1));
+	    EXPECT_FALSE(newSignature.test(3));
+	}
 
-    TEST_F(EntityManagerTest, EntityIdSequence) {
-        // Create multiple entities and check the ID sequence
-        Entity entity1 = entityManager->createEntity();
-        Entity entity2 = entityManager->createEntity();
-        Entity entity3 = entityManager->createEntity();
+	// Test get living entities
+	TEST_F(EntityManagerTest, GetLivingEntitiesReturnsCorrectEntities) {
+	    // Empty at start
+	    EXPECT_EQ(entityManager.getLivingEntities().size(), 0);
 
-        // IDs should be sequential starting from 0
-        EXPECT_EQ(entity1, 0);
-        EXPECT_EQ(entity2, 1);
-        EXPECT_EQ(entity3, 2);
-    }
+	    // Create some entities
+	    auto entities = createMultipleEntities(5);
 
-    TEST_F(EntityManagerTest, ComplexEntityRecycling) {
-        // Create several entities
-        std::vector<Entity> entities;
-        for (int i = 0; i < 5; ++i) {
-            entities.push_back(entityManager->createEntity());
-        }
+	    // Check living entities
+	    auto livingEntities = entityManager.getLivingEntities();
+	    EXPECT_EQ(livingEntities.size(), 5);
 
-        // Destroy entities in a non-sequential order
-        entityManager->destroyEntity(entities[2]); // Destroy entity with ID 2
-        entityManager->destroyEntity(entities[0]); // Destroy entity with ID 0
+	    // Verify all created entities are in the living set
+	    for (auto e : entities) {
+	        bool found = false;
+	        for (auto le : livingEntities) {
+	            if (le == e) {
+	                found = true;
+	                break;
+	            }
+	        }
+	        EXPECT_TRUE(found) << "Entity " << e << " not found in living entities";
+	    }
 
-        // Create new entities and check the recycling order
-        // Entities should be reused in FIFO order (first destroyed first reused)
-        Entity newEntity1 = entityManager->createEntity();
-        Entity newEntity2 = entityManager->createEntity();
+	    // Remove an entity
+	    entityManager.destroyEntity(entities[2]);
 
-        // The first available entity should be recycled first,
-        // which depends on the order they were added to the availability queue
-        EXPECT_EQ(newEntity1, 0); // ID 2 should be recycled first (FIFO)
-        EXPECT_EQ(newEntity2, 2); // ID 0 should be recycled next
+	    // Check updated living entities
+	    livingEntities = entityManager.getLivingEntities();
+	    EXPECT_EQ(livingEntities.size(), 4);
 
-        // Destroy all entities and check if count reaches zero
-        entityManager->destroyEntity(entities[1]);
-        entityManager->destroyEntity(entities[3]);
-        entityManager->destroyEntity(entities[4]);
-        entityManager->destroyEntity(newEntity1);
-        entityManager->destroyEntity(newEntity2);
+	    // Verify entity was removed
+	    bool found = false;
+	    for (auto le : livingEntities) {
+	        if (le == entities[2]) {
+	            found = true;
+	            break;
+	        }
+	    }
+	    EXPECT_FALSE(found) << "Destroyed entity " << entities[2] << " still found in living entities";
+	}
 
-        EXPECT_EQ(entityManager->getLivingEntityCount(), 0);
-    }
+	// Test get living entity count
+	TEST_F(EntityManagerTest, GetLivingEntityCountReturnsCorrectCount) {
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 0);
 
-    TEST_F(EntityManagerTest, EntityCountTracking) {
-        // Test edge cases in entity count tracking
+	    auto e1 = entityManager.createEntity();
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 1);
 
-        // Create MAX_ENTITIES entities
-        std::vector<Entity> entities;
-        for (Entity i = 0; i < MAX_ENTITIES; ++i) {
-            entities.push_back(entityManager->createEntity());
-        }
+	    auto e2 = entityManager.createEntity();
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 2);
 
-        EXPECT_EQ(entityManager->getLivingEntityCount(), MAX_ENTITIES);
+	    entityManager.destroyEntity(e1);
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 1);
 
-        // Destroy half of the entities
-        for (Entity i = 0; i < MAX_ENTITIES / 2; ++i) {
-            entityManager->destroyEntity(entities[i]);
-        }
+	    entityManager.destroyEntity(e2);
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 0);
+	}
 
-        if (MAX_ENTITIES % 2 == 0) {
-        	EXPECT_EQ(entityManager->getLivingEntityCount(), MAX_ENTITIES / 2);
-        } else {
-        	EXPECT_EQ(entityManager->getLivingEntityCount(), MAX_ENTITIES / 2 + 1);
-        }
+	// Complex scenario test
+	TEST_F(EntityManagerTest, ComplexEntityLifecycleScenario) {
+	    // Create a batch of entities
+	    std::vector<Entity> batch1 = createMultipleEntities(10);
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 10);
 
-        // Create half of them again
-        for (Entity i = 0; i < MAX_ENTITIES / 2; ++i) {
-            entityManager->createEntity();
-        }
+	    // Set signatures for some entities
+	    for (size_t i = 0; i < batch1.size(); i += 2) {
+	        Signature sig;
+	        sig.set(i % MAX_COMPONENT_TYPE);
+	        entityManager.setSignature(batch1[i], sig);
+	    }
 
-        EXPECT_EQ(entityManager->getLivingEntityCount(), MAX_ENTITIES);
-    }
+	    // Destroy some entities
+	    for (size_t i = 0; i < batch1.size(); i += 3) {
+	        entityManager.destroyEntity(batch1[i]);
+	    }
 
-    TEST_F(EntityManagerTest, DestroyAlreadyDestroyedEntity) {
-        // Create an entity
-        Entity entity = entityManager->createEntity();
-        EXPECT_EQ(entityManager->getLivingEntityCount(), 1);
+	    // Create new entities (should reuse some IDs)
+	    std::vector<Entity> batch2 = createMultipleEntities(5);
 
-        // Destroy it once
-        entityManager->destroyEntity(entity);
-        EXPECT_EQ(entityManager->getLivingEntityCount(), 0);
+	    // Verify expected count
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 11); // 10 - 4 + 5 = 11 (10 original - 4 destroyed + 5 new)
 
-        // Destroy it again - this should work without errors
-        // and not affect the living entity count (which should remain 0)
-        EXPECT_NO_THROW(entityManager->destroyEntity(entity));
-        EXPECT_EQ(entityManager->getLivingEntityCount(), 0);
-    }
+	    // Check recycled IDs have clean signatures
+	    for (Entity e : batch2) {
+	        Signature sig = entityManager.getSignature(e);
+	        EXPECT_EQ(sig.count(), 0) << "Recycled entity " << e << " has non-empty signature";
+	    }
 
-    TEST_F(EntityManagerTest, SignatureManipulation) {
-        // Create an entity
-        Entity entity = entityManager->createEntity();
+	    // Destroy all entities
+	    auto livingEntities = entityManager.getLivingEntities();
+		std::vector<Entity> toDestroy;
+		toDestroy.reserve(livingEntities.size());
+		for (Entity e : livingEntities) {
+			toDestroy.push_back(e);
+		}
+		for (Entity e : toDestroy) {
+			entityManager.destroyEntity(e);
+		}
 
-        // Initial signature should be empty
-        EXPECT_TRUE(entityManager->getSignature(entity).none());
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 0);
+	}
 
-        // Set some bits
-        Signature signature;
-        signature.set(0);
-        signature.set(5);
-        signature.set(10);
-        entityManager->setSignature(entity, signature);
+	// Edge case - destroy entity that's already destroyed
+	TEST_F(EntityManagerTest, DestroyAlreadyDestroyedEntity) {
+	    // This doesn't throw, just performs the operation
+	    Entity e = entityManager.createEntity();
+	    entityManager.destroyEntity(e);
 
-        // Check that signature was properly set
-        Signature retrieved = entityManager->getSignature(entity);
-        EXPECT_TRUE(retrieved.test(0));
-        EXPECT_TRUE(retrieved.test(5));
-        EXPECT_TRUE(retrieved.test(10));
-        EXPECT_FALSE(retrieved.test(1));
+	    // Should not throw, signature already reset
+	    entityManager.destroyEntity(e);
 
-        // Modify signature
-        Signature newSignature = retrieved;
-        newSignature.reset(0);
-        newSignature.set(3);
-        entityManager->setSignature(entity, newSignature);
+	    EXPECT_EQ(entityManager.getLivingEntityCount(), 0);
 
-        // Check updated signature
-        retrieved = entityManager->getSignature(entity);
-        EXPECT_FALSE(retrieved.test(0));
-        EXPECT_TRUE(retrieved.test(3));
-        EXPECT_TRUE(retrieved.test(5));
-        EXPECT_TRUE(retrieved.test(10));
-    }
+	    // Create a new entity, should still get the same ID back
+	    Entity newE = entityManager.createEntity();
+	    EXPECT_EQ(newE, e);
+	}
+
 }
