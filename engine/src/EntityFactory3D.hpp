6d68fe2696e32112293e7b323f1edb41c8e5574f
Thyodas <guillaume.hein@epitech.eu>
2025-04-14 23:25:39 +0900

    refactor(asset-ecs): remove unused functions and commented code from EntityFactory3D

--- engine/src/EntityFactory3D.hpp
+++ engine/src/EntityFactory3D.hpp
@@ -66,11 +66,3 @@ namespace nexo
 			static ecs::Entity createBillboard(const glm::vec3 &pos, const glm::vec3 &size, const components::Material &material);
     };
 }
-
-namespace nexo::utils
-{
-    std::shared_ptr<components::MeshNode> loadModel(const std::string& path);
-    std::shared_ptr<components::MeshNode> processNode(const std::string &path, aiNode const *node, const aiScene* scene);
-    components::Mesh processMesh(const std::string &path, aiMesh* mesh, const aiScene* scene);
-    glm::mat4 convertAssimpMatrixToGLM(const aiMatrix4x4& matrix);
-}
