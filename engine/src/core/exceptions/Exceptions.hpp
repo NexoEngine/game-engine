0d36b946621fa2efcec63b0558eb8a60a0135b6a
iMeaNz <mehdy.morvan@epitech.eu>
2025-04-15 09:32:00 +0900

    feat(ecs-opti): add exceptions for too many lights

--- engine/src/core/exceptions/Exceptions.hpp
+++ engine/src/core/exceptions/Exceptions.hpp
@@ -15,8 +15,10 @@
 
 #include <string>
 #include <source_location>
+#include <format>
 
 #include "Exception.hpp"
+#include "components/Light.hpp"
 
 namespace nexo::core {
     class FileNotFoundException final : public Exception {
@@ -38,5 +40,26 @@ namespace nexo::core {
      		explicit SceneManagerLifecycleException(const std::string &message,
                                            const std::source_location loc = std::source_location::current())
                 : Exception(message, loc) {}
+    };
+
+    class TooManyPointLightsException : public Exception {
+    	public:
+     		explicit TooManyPointLightsException(unsigned int sceneRendered, size_t nbPointLights,
+                                           const std::source_location loc = std::source_location::current())
+                : Exception(std::format("Too many point lights ({} > {}) in scene [{}]", nbPointLights, MAX_POINT_LIGHTS, sceneRendered), loc) {}
+    };
+
+    class TooManySpotLightsException : public Exception {
+    	public:
+     		explicit TooManySpotLightsException(unsigned int sceneRendered, size_t nbSpotLights,
+                                           const std::source_location loc = std::source_location::current())
+       : Exception(std::format("Too many spot lights ({} > {}) in scene [{}]", nbSpotLights, MAX_SPOT_LIGHTS, sceneRendered), loc) {}
+    };
+
+    class TooManyDirectionalLightsException : public Exception {
+    	public:
+     		explicit TooManyDirectionalLightsException(unsigned int sceneRendered, size_t nbDirectionalLights,
+                                           const std::source_location loc = std::source_location::current())
+       : Exception(std::format("Too many directional lights ({} > {}) in scene [{}]", nbDirectionalLights, MAX_DIRECTIONAL_LIGHTS, sceneRendered), loc) {}
     };
 }
