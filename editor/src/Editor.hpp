4465240bdf95912414cb8eae9192626e501423a1
iMeaNz <mehdy.morvan@epitech.eu>
2025-04-29 02:43:50 +0900

    refactor(document-windows): split into multiple method + fix: bottom does not process inputs anymore

--- editor/src/Editor.hpp
+++ editor/src/Editor.hpp
@@ -164,6 +164,11 @@ namespace nexo::editor {
              * sets a flag to signal that the editor should quit.
              */
             void drawMenuBar();
+            void drawShortcutBar(const std::vector<CommandInfo> &possibleCommands);
+            void drawBackground();
+
+            void handleGlobalCommands();
+            std::vector<CommandInfo> handleFocusedWindowCommands();
 
             bool m_quit = false;
             bool m_showDemoWindow = false;
