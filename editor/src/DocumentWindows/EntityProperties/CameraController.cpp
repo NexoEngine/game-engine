b7ef6a08a9b35bd39677893346e7be1975bee5fb
iMeaNz <mehdy.morvan@epitech.eu>
2025-05-01 00:25:14 +0900

    feat(undo-redo): add undo/redo on camera controller modification

--- editor/src/DocumentWindows/EntityProperties/CameraController.cpp
+++ editor/src/DocumentWindows/EntityProperties/CameraController.cpp
@@ -16,18 +16,27 @@
 #include "components/Camera.hpp"
 #include "ImNexo/Elements.hpp"
 #include "ImNexo/EntityProperties.hpp"
+#include "context/ActionManager.hpp"
+#include "context/actions/EntityActions.hpp"
 
 namespace nexo::editor {
 
 	void CameraController::show(const ecs::Entity entity)
 	{
         auto& controllerComponent = Application::getEntityComponent<components::PerspectiveCameraController>(entity);
+        static components::PerspectiveCameraController::Memento beforeState;
 
         if (ImNexo::Header("##ControllerNode", "Camera Controller"))
         {
             ImGui::Spacing();
 
-            ImNexo::CameraController(controllerComponent);
+            ImNexo::InteractionState state = ImNexo::CameraController(controllerComponent, beforeState);
+            if (state == ImNexo::InteractionState::RELEASED) {
+                auto afterState = controllerComponent.save();
+                auto action = std::make_unique<ComponentChangeAction<components::PerspectiveCameraController>>(entity, beforeState, afterState);
+                ActionManager::get().recordAction(std::move(action));
+                beforeState = components::PerspectiveCameraController::Memento{};
+            }
             ImGui::TreePop();
         }
 	}
