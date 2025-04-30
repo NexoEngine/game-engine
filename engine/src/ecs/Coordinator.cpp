2c33e6afb77516f2d44ad5de9fff12f20f39d28e
iMeaNz <mehdy.morvan@epitech.eu>
2025-04-30 21:24:28 +0900

    refactor(undo-redo): fix the meta helper + not using anymore lambdas functions map for add/save/restore

--- engine/src/ecs/Coordinator.cpp
+++ engine/src/ecs/Coordinator.cpp
@@ -77,34 +77,9 @@ namespace nexo::ecs {
         return components;
     }
 
-    bool Coordinator::supportsMementoPattern(const std::any& component) const
+    bool Coordinator::supportsMementoPattern(const std::type_index typeIndex) const
     {
-        auto typeId = std::type_index(component.type());
-        auto it = m_supportsMementoPattern.find(typeId);
+        auto it = m_supportsMementoPattern.find(typeIndex);
         return (it != m_supportsMementoPattern.end()) && it->second;
-    }
-
-    std::any Coordinator::saveComponent(const std::any& component) const
-    {
-        auto typeId = std::type_index(component.type());
-        auto it = m_saveComponentFunctions.find(typeId);
-        if (it != m_saveComponentFunctions.end())
-            return it->second(component);
-        return std::any();
-    }
-
-    std::any Coordinator::restoreComponent(const std::any& memento, const std::type_index& componentType) const
-    {
-        auto it = m_restoreComponentFunctions.find(componentType);
-        if (it != m_restoreComponentFunctions.end())
-            return it->second(memento);
-        return std::any();
-    }
-
-    void Coordinator::addComponentAny(Entity entity, const std::type_index& typeIndex, const std::any& component)
-    {
-        auto it = m_addComponentFunctions.find(typeIndex);
-        if (it != m_addComponentFunctions.end())
-            it->second(entity, component);
     }
 }
