1903e68002207a24d82a4cb7bda03c1787d2826b
iMeaNz <mehdy.morvan@epitech.eu>
2025-05-01 17:30:25 +0900

    feat(undo-redo): add undo/redo on camera property

--- editor/src/DocumentWindows/EntityProperties/CameraProperty.cpp
+++ editor/src/DocumentWindows/EntityProperties/CameraProperty.cpp
@@ -13,19 +13,34 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "CameraProperty.hpp"
+#include "ImNexo/ImNexo.hpp"
 #include "components/Camera.hpp"
 #include "ImNexo/Elements.hpp"
 #include "ImNexo/EntityProperties.hpp"
+#include "context/ActionManager.hpp"
+#include "context/actions/EntityActions.hpp"
 
 namespace nexo::editor {
 
 	void CameraProperty::show(const ecs::Entity entity)
 	{
         auto& cameraComponent = Application::getEntityComponent<components::CameraComponent>(entity);
+        static components::CameraComponent::Memento beforeState;
 
         if (ImNexo::Header("##CameraNode", "Camera"))
         {
+            auto cameraComponentCopy = cameraComponent;
+            ImNexo::resetItemStates();
             ImNexo::Camera(cameraComponent);
+            if (ImNexo::isItemActivated()) {
+                beforeState = cameraComponentCopy.save();
+            } else if (ImNexo::isItemDeactivated()) {
+                cameraComponent = cameraComponentCopy;
+                auto afterState = cameraComponent.save();
+                auto action = std::make_unique<ComponentChangeAction<components::CameraComponent>>(entity, beforeState, afterState);
+                ActionManager::get().recordAction(std::move(action));
+                beforeState = components::CameraComponent::Memento{};
+            }
             ImGui::TreePop();
         }
 	}
