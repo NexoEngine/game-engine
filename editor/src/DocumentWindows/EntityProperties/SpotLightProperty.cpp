9a2694238eafdef58b7230bde45782c9737a91c6
iMeaNz <mehdy.morvan@epitech.eu>
2025-05-01 19:48:31 +0900

    feat(undo-redo): add undo/redo for spot light property

--- editor/src/DocumentWindows/EntityProperties/SpotLightProperty.cpp
+++ editor/src/DocumentWindows/EntityProperties/SpotLightProperty.cpp
@@ -13,9 +13,14 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "SpotLightProperty.hpp"
+#include "ImNexo/EntityProperties.hpp"
+#include "ImNexo/ImNexo.hpp"
 #include "components/Light.hpp"
+#include "components/Transform.hpp"
+#include "context/actions/EntityActions.hpp"
 #include "math/Light.hpp"
 #include "ImNexo/Widgets.hpp"
+#include "context/ActionManager.hpp"
 
 namespace nexo::editor {
 
@@ -24,55 +29,28 @@ namespace nexo::editor {
         auto& spotComponent = Application::getEntityComponent<components::SpotLightComponent>(entity);
         auto &transformComponent = Application::getEntityComponent<components::TransformComponent>(entity);
 
+        static components::SpotLightComponent::Memento beforeStateSpot;
+        static components::TransformComponent::Memento beforeStateTransform;
+
         if (ImNexo::Header("##SpotNode", "Spot light"))
         {
-            ImGui::Spacing();
-        	static ImGuiColorEditFlags colorPickerMode = ImGuiColorEditFlags_PickerHueBar;
-			static bool showColorPicker = false;
-			ImGui::Text("Color");
-			ImGui::SameLine();
-			glm::vec4 color = {spotComponent.color, 1.0f};
-			ImNexo::ColorEditor("##ColorEditor Spot light", &color, &colorPickerMode, &showColorPicker);
-			spotComponent.color = color;
-
-			ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(5.0f, 10.0f));
-   			if (ImGui::BeginTable("InspectorSpotTable", 4,
-                ImGuiTableFlags_SizingStretchProp))
-            {
-                ImGui::TableSetupColumn("##Label", ImGuiTableColumnFlags_WidthStretch | ImGuiTableColumnFlags_NoHeaderLabel);
-                ImGui::TableSetupColumn("##X", ImGuiTableColumnFlags_WidthStretch | ImGuiTableColumnFlags_NoHeaderLabel);
-                ImGui::TableSetupColumn("##Y", ImGuiTableColumnFlags_WidthStretch | ImGuiTableColumnFlags_NoHeaderLabel);
-                ImGui::TableSetupColumn("##Z", ImGuiTableColumnFlags_WidthStretch | ImGuiTableColumnFlags_NoHeaderLabel);
-
-                ImNexo::RowDragFloat3("Direction", "X", "Y", "Z", &spotComponent.direction.x, -FLT_MAX, FLT_MAX, 0.1f);
-                ImNexo::RowDragFloat3("Position", "X", "Y", "Z", &transformComponent.pos.x, -FLT_MAX, FLT_MAX, 0.1f);
-
-
-                ImGui::EndTable();
+            ImNexo::resetItemStates();
+            auto spotComponentCopy = spotComponent;
+            auto transformComponentCopy = transformComponent;
+            ImNexo::SpotLight(spotComponent, transformComponent);
+            if (ImNexo::isItemActivated()) {
+                beforeStateSpot = spotComponentCopy.save();
+                beforeStateTransform = transformComponentCopy.save();
+            } else if (ImNexo::isItemDeactivated()) {
+                auto afterStateSpot = spotComponent.save();
+                auto afterStateTransform = transformComponent.save();
+                auto actionGroup = ActionManager::get().createActionGroup();
+                auto spotAction = std::make_unique<ComponentChangeAction<components::SpotLightComponent>>(entity, beforeStateSpot, afterStateSpot);
+                auto transformAction = std::make_unique<ComponentChangeAction<components::TransformComponent>>(entity, beforeStateTransform, afterStateTransform);
+                actionGroup->addAction(std::move(spotAction));
+                actionGroup->addAction(std::move(transformAction));
+                ActionManager::get().recordAction(std::move(actionGroup));
             }
-
-            if (ImGui::BeginTable("InspectorCutOffSpotTable", 2, ImGuiTableFlags_SizingStretchProp))
-            {
-	            ImGui::TableSetupColumn("##Label", ImGuiTableColumnFlags_WidthStretch | ImGuiTableColumnFlags_NoHeaderLabel);
-	            ImGui::TableSetupColumn("##X", ImGuiTableColumnFlags_WidthStretch | ImGuiTableColumnFlags_NoHeaderLabel);
-
-                if (ImNexo::RowDragFloat1("Distance", "", &spotComponent.maxDistance, 1.0f, 3250.0f, 1.0f))
-                {
-					auto [lin, quad] = math::computeAttenuationFromDistance(spotComponent.maxDistance);
-					spotComponent.linear = lin;
-					spotComponent.quadratic = quad;
-                }
-                float innerCutOffDegrees = glm::degrees(glm::acos(spotComponent.cutOff));
-                float outerCutOffDegrees = glm::degrees(glm::acos(spotComponent.outerCutoff));
-                if (ImNexo::RowDragFloat1("Inner cut off", "", &innerCutOffDegrees, 0.0f, outerCutOffDegrees, 0.5f))
-                	spotComponent.cutOff = glm::cos(glm::radians(innerCutOffDegrees));
-                if (ImNexo::RowDragFloat1("Outer cut off", "", &outerCutOffDegrees, innerCutOffDegrees, 90.0f, 0.5f))
-                	spotComponent.outerCutoff = glm::cos(glm::radians(outerCutOffDegrees));
-
-                ImGui::EndTable();
-            }
-
-            ImGui::PopStyleVar();
         	ImGui::TreePop();
         }
 	}
