383945cb421a8aeb90d2c6db0847e02bb6dee390
iMeaNz <mehdy.morvan@epitech.eu>
2025-05-01 17:43:20 +0900

    feat(undo-redo): add camera target property

--- editor/src/DocumentWindows/EntityProperties/CameraTarget.cpp
+++ editor/src/DocumentWindows/EntityProperties/CameraTarget.cpp
@@ -14,20 +14,33 @@
 
 #include "CameraTarget.hpp"
 #include "Definitions.hpp"
+#include "ImNexo/ImNexo.hpp"
 #include "components/Camera.hpp"
 #include "ImNexo/Elements.hpp"
 #include "ImNexo/EntityProperties.hpp"
+#include "context/ActionManager.hpp"
 
 namespace nexo::editor {
 
     void CameraTarget::show(const ecs::Entity entity)
     {
         auto& targetComponent = Application::getEntityComponent<components::PerspectiveCameraTarget>(entity);
+        static components::PerspectiveCameraTarget::Memento beforeState;
 
-        if (ImNexo::Header("##ControllerNode", "Camera Controller"))
+        if (ImNexo::Header("##TargetNode", "Camera Target"))
         {
+            auto targetComponentCopy = targetComponent;
             ImGui::Spacing();
-            ImNexo::CameraTarget(targetComponent),
+            ImNexo::resetItemActiveState();
+            ImNexo::CameraTarget(targetComponent);
+            if (ImNexo::isItemActivated()) {
+                beforeState = targetComponentCopy.save();
+            } else if (ImNexo::isItemDeactivated()) {
+                auto afterState = targetComponent.save();
+                auto action = std::make_unique<ComponentChangeAction<components::PerspectiveCameraTarget>>(entity, beforeState, afterState);
+                ActionManager::get().recordAction(std::move(action));
+                beforeState = components::PerspectiveCameraTarget::Memento{};
+            }
             ImGui::TreePop();
         }
     }
