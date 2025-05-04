7ef9f05d37890e8a6ab236024f241d6e33984b22
iMeaNz <mehdy.morvan@epitech.eu>
2025-05-05 08:10:45 +0900

    feat(test-window): add test window + test protocol file parser

---
+++ editor/src/DocumentWindows/TestWindow/Update.cpp
@@ -0,0 +1,24 @@
+//// Update.cpp ///////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Mehdy MORVAN
+//  Date:        05/05/2025
+//  Description: Source file for the test window update
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "TestWindow.hpp"
+
+namespace nexo::editor {
+
+    void TestWindow::update()
+    {
+
+    }
+
+}
