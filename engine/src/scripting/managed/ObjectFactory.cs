41c24db386b0f000f3af4124045bf5d1d3fcf392
Thyodas <guillaume.hein@epitech.eu>
2025-05-05 00:34:50 +0900

    feat(scripting): add start of C# lib

---
+++ engine/src/scripting/managed/ObjectFactory.cs
@@ -0,0 +1,85 @@
+﻿//// ObjectFactory.cs /////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Guillaume HEIN
+//  Date:        03/05/2025
+//  Description: Source file for the scripting object factory
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace Nexo
+{
+    public static class ObjectFactory
+    {
+        private static Dictionary<int, object> _instances = new Dictionary<int, object>();
+        private static int _nextId = 1;
+        
+        // Generic factory method using Activator
+        public static int CreateInstance(string typeName)
+        {
+            try 
+            {
+                Type type = Type.GetType(typeName);
+                if (type == null)
+                    return -1;
+                    
+                object instance = Activator.CreateInstance(type);
+                int id = _nextId++;
+                _instances[id] = instance;
+                return id;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+        
+        // Create with parameters
+        public static int CreateInstanceWithParams(string typeName, object[] parameters)
+        {
+            try 
+            {
+                Type type = Type.GetType(typeName);
+                if (type == null)
+                    return -1;
+                    
+                object instance = Activator.CreateInstance(type, parameters);
+                int id = _nextId++;
+                _instances[id] = instance;
+                return id;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+        
+        // Method to release the instance
+        public static bool ReleaseInstance(int id)
+        {
+            return _instances.Remove(id);
+        }
+        
+        // Helper to invoke methods on instances
+        public static bool InvokeMethod(int id, string methodName, object[] parameters)
+        {
+            if (!_instances.TryGetValue(id, out object instance))
+                return false;
+                
+            try
+            {
+                instance.GetType().GetMethod(methodName).Invoke(instance, parameters);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
