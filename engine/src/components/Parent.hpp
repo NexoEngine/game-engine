d33c0258e1e05b6373167ad08079b0983b63b85e
iMeaNz <mehdy.morvan@epitech.eu>
2025-05-07 05:50:51 +0900

    feat(mesh): add static mesh, model and parent components (not used yet)

---
+++ engine/src/components/Parent.hpp
@@ -0,0 +1,24 @@
+//// Parent.hpp ///////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Mehdy MORVAN
+//  Date:        06/05/2025
+//  Description: Header file for the parent component
+//
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "ecs/Definitions.hpp"
+
+namespace nexo::components {
+
+    struct ParentComponent {
+        ecs::Entity parent;
+    };
+
+}
