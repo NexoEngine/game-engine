1f2ff1a1789cded747cabb1f2d541316fa7e9ccf
Thyodas <guillaume.hein@epitech.eu>
2025-05-08 23:27:46 +0900

    feat(scripting): enhance native interop with new API functions and logging capabilities + can create cube from C#

---
+++ engine/src/scripting/managed/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Nexo;
+
+public enum LogLevel : UInt32
+{
+    Fatal,
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Dev,
+    User
+}
+
+public static class Logger
+{
+    /// <summary>
+    /// Logs a message with the specified log level.
+    /// </summary>
+    /// <param name="level">Specifies the log level (e.g., Fatal, Error, Warn, Info, Debug, Dev, User).</param>
+    /// <param name="message">The message to be logged.</param>
+    public static void Log(LogLevel level, string message) => NativeInterop.NxLog((uint)level, message);
+    
+}
