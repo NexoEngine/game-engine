580703da297256653fd3e971f3971247f1535742
Thyodas <guillaume.hein@epitech.eu>
2025-05-09 06:34:40 +0900

    chore(logging): add header comments to Logger.cs for documentation

--- engine/src/scripting/managed/Logger.cs
+++ engine/src/scripting/managed/Logger.cs
@@ -1,4 +1,18 @@
-﻿using System.Runtime.InteropServices;
+﻿//// Logger.cs ////////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Guillaume HEIN
+//  Date:        09/05/2025
+//  Description: Source file for the logger class
+//
+///////////////////////////////////////////////////////////////////////////////
+
+using System.Runtime.InteropServices;
 
 namespace Nexo;
 
@@ -20,6 +34,6 @@ public static class Logger
     /// </summary>
     /// <param name="level">Specifies the log level (e.g., Fatal, Error, Warn, Info, Debug, Dev, User).</param>
     /// <param name="message">The message to be logged.</param>
-    public static void Log(LogLevel level, string message) => NativeInterop.NxLog((uint)level, message);
+    public static void Log(LogLevel level, String message) => NativeInterop.NxLog((UInt32)level, message);
     
 }
