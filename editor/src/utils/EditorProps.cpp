4e11779c91fddb21f8426998f46bee35bbcd52b2
iMeaNz <mehdy.morvan@epitech.eu>
2025-05-14 13:30:25 +0900

    refactor(mesh): materials are now assets

--- editor/src/utils/EditorProps.cpp
+++ editor/src/utils/EditorProps.cpp
@@ -27,52 +27,76 @@ namespace nexo::editor::utils {
     static void addCameraProps(const ecs::Entity entity)
     {
         auto& catalog = assets::AssetCatalog::getInstance();
-        components::Material billboardMat{};
-        billboardMat.isOpaque = false;
+
         static const assets::AssetRef<assets::Texture> cameraIconTexture = catalog.createAsset<assets::Texture>(
             assets::AssetLocation("_internal::cameraIcon@_internal"),
             Path::resolvePathRelativeToExe("../resources/textures/cameraIcon.png")
         );
-        billboardMat.albedoTexture = cameraIconTexture;
-        billboardMat.shader = "Albedo unshaded transparent";
+        std::unique_ptr<components::Material> billboardMat = std::make_unique<components::Material>();
+        billboardMat->isOpaque = false;
+        billboardMat->albedoTexture = cameraIconTexture;
+        billboardMat->shader = "Albedo unshaded transparent";
+        const auto materialRef = assets::AssetCatalog::getInstance().createAsset<assets::Material>(
+                                assets::AssetLocation("_internal::CameraPropMat@_internal"),
+                                std::move(billboardMat));
+        components::MaterialComponent matComponent;
+        matComponent.material = materialRef;
+
         components::BillboardComponent billboardMesh;
         billboardMesh.vao = renderer::NxRenderer3D::getBillboardVAO();
+
         Application::m_coordinator->addComponent(entity, billboardMesh);
-        Application::m_coordinator->addComponent(entity, billboardMat);
+        Application::m_coordinator->addComponent(entity, matComponent);
     }
 
     static void addPointLightProps(const ecs::Entity entity)
     {
         auto& catalog = assets::AssetCatalog::getInstance();
-        components::Material billboardMat{};
-        billboardMat.isOpaque = false;
+
         static const assets::AssetRef<assets::Texture> pointLightIconTexture = catalog.createAsset<assets::Texture>(
             assets::AssetLocation("_internal::pointLightIcon@_internal"),
             Path::resolvePathRelativeToExe("../resources/textures/pointLightIcon.png")
         );
-        billboardMat.albedoTexture = pointLightIconTexture;
-        billboardMat.shader = "Albedo unshaded transparent";
+        std::unique_ptr<components::Material> billboardMat = std::make_unique<components::Material>();
+        billboardMat->isOpaque = false;
+        billboardMat->albedoTexture = pointLightIconTexture;
+        billboardMat->shader = "Albedo unshaded transparent";
+        const auto materialRef = assets::AssetCatalog::getInstance().createAsset<assets::Material>(
+                                assets::AssetLocation("_internal::CameraPropMat@_internal"),
+                                std::move(billboardMat));
+        components::MaterialComponent matComponent;
+        matComponent.material = materialRef;
+
         components::BillboardComponent billboardMesh;
         billboardMesh.vao = renderer::NxRenderer3D::getBillboardVAO();
+
         Application::m_coordinator->addComponent(entity, billboardMesh);
-        Application::m_coordinator->addComponent(entity, billboardMat);
+        Application::m_coordinator->addComponent(entity, matComponent);
     }
 
     static void addSpotLightProps(const ecs::Entity entity)
     {
         auto& catalog = assets::AssetCatalog::getInstance();
-        components::Material billboardMat{};
-        billboardMat.isOpaque = false;
+
         static const assets::AssetRef<assets::Texture> spotLightIconTexture = catalog.createAsset<assets::Texture>(
             assets::AssetLocation("_internal::spotLightIcon@_internal"),
             Path::resolvePathRelativeToExe("../resources/textures/spotLightIcon.png")
         );
-        billboardMat.albedoTexture = spotLightIconTexture;
-        billboardMat.shader = "Albedo unshaded transparent";
+        std::unique_ptr<components::Material> billboardMat = std::make_unique<components::Material>();
+        billboardMat->isOpaque = false;
+        billboardMat->albedoTexture = spotLightIconTexture;
+        billboardMat->shader = "Albedo unshaded transparent";
+        const auto materialRef = assets::AssetCatalog::getInstance().createAsset<assets::Material>(
+                                assets::AssetLocation("_internal::CameraPropMat@_internal"),
+                                std::move(billboardMat));
+        components::MaterialComponent matComponent;
+        matComponent.material = materialRef;
+
         components::BillboardComponent billboardMesh;
         billboardMesh.vao = renderer::NxRenderer3D::getBillboardVAO();
+
         Application::m_coordinator->addComponent(entity, billboardMesh);
-        Application::m_coordinator->addComponent(entity, billboardMat);
+        Application::m_coordinator->addComponent(entity, matComponent);
     }
 
     void addPropsTo(const ecs::Entity entity, const PropsType type)
---
+++ engine/src/components/MaterialComponent.hpp
@@ -0,0 +1,22 @@
+//// MaterialComponent.hpp ///////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Mehdy MORVAN
+//  Date:        14/05/2025
+//  Description: Header file for the material component
+//
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "assets/Assets/Material/Material.hpp"
+
+namespace nexo::components {
+    struct MaterialComponent {
+        assets::AssetRef<assets::Material> material;
+    };
+}
