ade3beb9355dda2aa0fbcf0dbb397c6fb5d65190
iMeaNz <mehdy.morvan@epitech.eu>
2025-05-14 11:48:46 +0900

    style(mesh): fix header

--- engine/src/renderer/Attributes.hpp
+++ engine/src/renderer/Attributes.hpp
@@ -1,4 +1,4 @@
-//// Attributes.hpp ///////////////////////////////////////////////////////////////
+//// Attributes.hpp ///////////////////////////////////////////////////////////
 //
 //  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
 //  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
