8ca0b4561aecdc1530cd8d90eeea03a5b0dd112f
iMeaNz <mehdy.morvan@epitech.eu>
2025-05-14 11:51:21 +0900

    feat(mesh): add forward, mask, outline and grid render passes

---
+++ engine/src/renderPasses/GridPass.cpp
@@ -0,0 +1,73 @@
+//// GridPass.cpp /////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Mehdy MORVAN
+//  Date:        09/05/2025
+//  Description: Source file for the grid render pass
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "GridPass.hpp"
+#include "DrawCommand.hpp"
+#include "Framebuffer.hpp"
+#include "RenderCommand.hpp"
+#include "renderer/RenderPipeline.hpp"
+#include "Masks.hpp"
+
+namespace nexo::renderer {
+    GridPass::GridPass(unsigned int width, unsigned int height) : RenderPass(GRID_PASS)
+    {
+        renderer::NxFramebufferSpecs framebufferSpecs;
+        framebufferSpecs.attachments = {
+            renderer::NxFrameBufferTextureFormats::RGBA8,
+            renderer::NxFrameBufferTextureFormats::RED_INTEGER,
+            renderer::NxFrameBufferTextureFormats::Depth
+        };
+        framebufferSpecs.width = width;
+        framebufferSpecs.height = height;
+        m_output = renderer::NxFramebuffer::create(framebufferSpecs);
+    }
+
+    void GridPass::execute(RenderPipeline& pipeline)
+    {
+        auto output = (m_isFinal) ? pipeline.getFinalRenderTarget() : m_output;
+        std::shared_ptr<NxFramebuffer> prevPass = nullptr;
+        for (const auto &prereq : prerequisites) {
+            const auto &prereqPass = pipeline.getRenderPass(prereq);
+            prevPass = pipeline.getOutput(prereqPass->id);
+            break;
+        }
+        if (!prevPass)
+            return;
+
+        //output->bind();
+        output->copy(prevPass);
+        output->bind();
+
+        // Render the grid
+        renderer::NxRenderCommand::setDepthMask(false);
+        renderer::NxRenderCommand::setCulling(false);
+        const auto &drawCommands = pipeline.getDrawCommands();
+        for (const auto &cmd : drawCommands) {
+            if (cmd.filterMask & F_GRID_PASS) {
+                cmd.execute();
+            }
+        }
+        renderer::NxRenderCommand::setDepthMask(true);
+        renderer::NxRenderCommand::setCulling(true);
+        renderer::NxRenderCommand::setCulledFace(CulledFace::BACK);
+
+        output->unbind();
+        pipeline.setOutput(id, output);
+    }
+
+    void GridPass::resize(unsigned int width, unsigned int height)
+    {
+        m_output->resize(width, height);
+    }
+}
