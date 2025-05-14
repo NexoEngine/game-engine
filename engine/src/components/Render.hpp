71b63ef41cd208565f9b9444a841e15ad6fc216d
iMeaNz <mehdy.morvan@epitech.eu>
2025-05-14 11:44:12 +0900

    refactor(mesh): render component now only has metadata info about the entity (no more shapes)

--- engine/src/components/Render.hpp
+++ engine/src/components/Render.hpp
@@ -13,129 +13,37 @@
 ///////////////////////////////////////////////////////////////////////////////
 #pragma once
 
-#include "Render2D.hpp"
-#include "Render3D.hpp"
-#include "Transform.hpp"
-#include "Shapes2D.hpp"
-#include "Shapes3D.hpp"
-
-#include <memory>
-#include <utility>
-
 namespace nexo::components
 {
-    enum class RenderType
+    enum class PrimitiveType
     {
-        RENDER_2D,
-        RENDER_3D
-    };
-
-    struct Renderable
-    {
-        virtual ~Renderable() = default;
-
-        bool isRendered = true;
-
-        virtual void draw(std::shared_ptr<renderer::NxRendererContext> &context,
-                          const TransformComponent &transf, int entityID) const = 0;
-        [[nodiscard]] virtual std::shared_ptr<Renderable> clone() const = 0;
-    };
-
-    struct Renderable2D final : Renderable
-    {
-        SpriteComponent sprite;
-        std::shared_ptr<Shape2D> shape;
-
-        explicit Renderable2D(SpriteComponent sprite,
-                              const std::shared_ptr<Shape2D>& shape) : sprite(std::move(sprite)), shape(shape)
-        {
-        };
-
-        void draw(std::shared_ptr<renderer::NxRendererContext> &context,
-                  const TransformComponent &transform, int entityID) const override
-        {
-            shape->draw(context, transform, sprite, entityID);
-        }
-
-        [[nodiscard]] std::shared_ptr<Renderable> clone() const override
-        {
-            //std::shared_ptr<Shape2D> clonedShape = shape ? shape->clone() : nullptr;
-            //return std::make_shared<Renderable2D>(sprite, clonedShape);
-            return nullptr;
-        }
-    };
-
-    struct Renderable3D final : Renderable {
-        Material material; // TODO: replace with AssetRef
-        std::shared_ptr<Shape3D> shape;
-
-        explicit Renderable3D(Material  material,
-                              const std::shared_ptr<Shape3D> &shape) : material(std::move(material)), shape(shape) {};
-
-        void draw(std::shared_ptr<renderer::NxRendererContext> &context, const TransformComponent &transf, int entityID) const override
-        {
-            shape->draw(context, transf, material, entityID);
-        }
-
-        [[nodiscard]] std::shared_ptr<Renderable> clone() const override
-        {
-            std::shared_ptr<Shape3D> clonedShape = shape ? shape->clone() : nullptr;
-            return std::make_shared<Renderable3D>(material, clonedShape);
-        }
+        CUBE,
+        MESH,
     };
 
     struct RenderComponent {
         bool isRendered = true;
-        RenderType type = RenderType::RENDER_2D;
-
-        std::shared_ptr<Renderable> renderable;
+        PrimitiveType type = PrimitiveType::MESH;
 
         RenderComponent() = default;
 
-        explicit RenderComponent(const std::shared_ptr<Renderable>& renderable, const RenderType type)
-            : type(type), renderable(renderable)
-        {
-        }
-
-        void draw(std::shared_ptr<renderer::NxRendererContext> &context, const TransformComponent &transform, const int entityID = -1) const
-        {
-            if (isRendered && renderable)
-                renderable->draw(context, transform, entityID);
-        }
-
         struct Memento {
-            bool isRendered = true;
-            RenderType type = RenderType::RENDER_2D;
-
-            std::shared_ptr<Renderable> renderable;
+            bool isRendered;
+            PrimitiveType type;
         };
 
         void restore(const Memento &memento)
         {
             isRendered = memento.isRendered;
             type = memento.type;
-            renderable = memento.renderable;
         }
 
         [[nodiscard]] Memento save() const
         {
             return {
                 isRendered,
-                type,
-                renderable ? renderable->clone() : nullptr
+                type
             };
-        }
-
-        [[nodiscard]] RenderComponent clone() const
-        {
-            RenderComponent copy;
-            copy.isRendered = isRendered;
-            copy.type = type;
-            if (renderable)
-                copy.renderable = renderable->clone();
-            else
-                copy.renderable = nullptr;
-            return copy;
         }
     };
 }
