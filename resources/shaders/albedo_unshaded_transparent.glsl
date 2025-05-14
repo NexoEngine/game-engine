3d5d2aee150af05c8bbb4dcd383cc77845b4c2d3
iMeaNz <mehdy.morvan@epitech.eu>
2025-05-14 11:55:33 +0900

    refactor(mesh): now using entity id as uniform

--- resources/shaders/albedo_unshaded_transparent.glsl
+++ resources/shaders/albedo_unshaded_transparent.glsl
@@ -5,19 +5,16 @@ layout(location = 1) in vec2 aTexCoord;
 layout(location = 2) in vec3 aNormal;
 layout(location = 3) in vec3 aTangent;
 layout(location = 4) in vec3 aBiTangent;
-layout(location = 5) in int aEntityID;
 
 uniform mat4 uViewProjection;
 uniform mat4 uMatModel;
 
 out vec2 vTexCoord;
-flat out int vEntityID;
 
 void main()
 {
     vec4 worldPos = uMatModel * vec4(aPos, 1.0);
     vTexCoord = aTexCoord;
-    vEntityID = aEntityID;
     gl_Position = uViewProjection * worldPos;
 }
 
@@ -27,7 +24,6 @@ layout(location = 0) out vec4 FragColor;
 layout(location = 1) out int EntityID;
 
 in vec2 vTexCoord;
-flat in int vEntityID;
 
 struct Material {
     vec4 albedoColor;
@@ -37,11 +33,13 @@ uniform Material uMaterial;
 
 uniform sampler2D uTexture[32];
 
+uniform int uEntityId;
+
 void main()
 {
     if (texture(uTexture[uMaterial.albedoTexIndex], vTexCoord).a < 0.1)
         discard;
     vec3 color = uMaterial.albedoColor.rgb * vec3(texture(uTexture[uMaterial.albedoTexIndex], vTexCoord));
     FragColor = vec4(color, 1.0);
-    EntityID = vEntityID;
+    EntityID = uEntityId;
 }
