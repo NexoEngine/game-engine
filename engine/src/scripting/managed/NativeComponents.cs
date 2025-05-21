ff13a09d039ee41e41f760e6c60e5946b46c006e
Thomas Parenteau <thomas.parenteau@epitech.eu>
2025-05-21 17:42:31 +0200

    feat: trygetcomponentbyid, issue #284
    Not final version, have to refactor getComponentTypeFromNativeId on NativeApi.cpp to remove the switch,

---
+++ engine/src/scripting/managed/NativeComponents.cs
@@ -0,0 +1,29 @@
+﻿//// NativeComponents.cs //////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Thomas PARENTEAU
+//  Date:        20/05/2025
+//  Description: Enum for NativeComponents, used in GetComponent
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace Nexo;
+
+/// <summary>
+/// Enum representing the components available in C++ accessible via interop.
+/// Must exactly match the NativeComponents enum defined in ManagedTypedef.hpp on the C++ side.
+/// </summary>
+
+public enum NativeComponents : uint
+{
+    Transform = 0,
+    AmbientLight = 1,
+    DirectionalLight = 2,
+    PointLight = 3,
+    SpotLight = 4,
+}
