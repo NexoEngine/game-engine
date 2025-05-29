de17bc3f859d1594159fb63addb79fba7b389315
Marie Giacomel <marie.giacomel@epitech.eu>
2025-05-29 17:38:38 -0400

    fix: sphere primitive with custom nbSubdivision

--- engine/src/EntityFactory3D.cpp
+++ engine/src/EntityFactory3D.cpp
@@ -334,9 +334,7 @@ namespace nexo
         transform.quat = glm::quat(rotation);
 
         components::StaticMeshComponent mesh;
-        mesh.vao = renderer::NxRenderer3D::getSphereVAO(0);
-        std::cout << "1 sphere vao: vertices " << mesh.vao->getVertexBuffers().size()
-          << " indices: " << mesh.vao->getIndexBuffer()->getCount() << std::endl;
+        mesh.vao = renderer::NxRenderer3D::getSphereVAO(2);
 
         auto material = std::make_unique<components::Material>();
         material->albedoColor = color;
@@ -367,8 +365,6 @@ namespace nexo
 
         components::StaticMeshComponent mesh;
         mesh.vao = renderer::NxRenderer3D::getSphereVAO(0);
-        std::cout << "2 sphere vao: vertices " << mesh.vao->getVertexBuffers().size()
-                  << " indices: " << mesh.vao->getIndexBuffer()->getCount() << std::endl;
 
         const auto materialRef = assets::AssetCatalog::getInstance().createAsset<assets::Material>(
             assets::AssetLocation("_internal::CubeMat@_internal"),
