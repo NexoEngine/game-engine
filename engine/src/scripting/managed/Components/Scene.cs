e1ee7e08c0dec9e13a0d32bde0da6b15cbc87061
Thomas Parenteau <thomas.parenteau@epitech.eu>
2025-06-03 16:45:55 +0200

    feat(scripting): more components c# side

---
+++ engine/src/scripting/managed/Components/Scene.cs
@@ -0,0 +1,30 @@
+﻿//// Scene.cs /////////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Thomas PARENTEAU
+//  Date:        03/06/2025
+//  Description: Source file for the SceneTag component in C#.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+using System.Runtime.InteropServices;
+
+namespace Nexo.Components
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SceneTag
+    {
+        public uint id;
+
+        [MarshalAs(UnmanagedType.I1)]
+        public bool isActive;
+
+        [MarshalAs(UnmanagedType.I1)]
+        public bool isRendered;
+    }
+}
