f75cff2d82235fb09993ea32eb5829ea5eb20ad1
Thomas Parenteau <thomas.parenteau@epitech.eu>
2025-06-12 12:14:36 +0200

    fix(scripting-api): miss spelled component c# side and fix addcomponent
    Add setrestorecomponent to the ecs and link it to all component (same as the registercomponent)

--- engine/src/scripting/managed/NativeInterop.cs
+++ engine/src/scripting/managed/NativeInterop.cs
@@ -26,10 +26,10 @@ namespace Nexo
     public struct ComponentTypeIds
     {
         public UInt32 Transform;
-        public UInt32 AmbientLight;
-        public UInt32 DirectionalLight;
-        public UInt32 PointLight;
-        public UInt32 SpotLight;
+        public UInt32 AmbientLightComponent;
+        public UInt32 DirectionalLightComponent;
+        public UInt32 PointLightComponent;
+        public UInt32 SpotLightComponent;
         public UInt32 RenderComponent;
         public UInt32 SceneTag;
         public UInt32 CameraComponent;
@@ -312,6 +312,7 @@ namespace Nexo
             UInt32 cubeId = CreateCube(new Vector3(1, 4.2f, 3), new Vector3(1, 1, 1), new Vector3(7, 8, 9), new Vector4(1, 0, 0, 1));
             _cubeId = cubeId;
             Console.WriteLine($"Created cube with ID: {cubeId}");
+            NativeInterop.AddComponent<CameraComponent>(cubeId);
             
             // AddComponent test
             AddComponent<CameraComponent>(cubeId);
