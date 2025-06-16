db83f1a0bdf90444ecf32dfb17e46db516e54609
Marie Giacomel <marie.giacomel@epitech.eu>
2025-06-16 12:39:08 -0700

    feat: remove popup for primitive in sceneTree

---
+++ editor/src/DocumentWindows/PrimitiveWindow/Shutdown.cpp
@@ -0,0 +1,24 @@
+//// Shutdown.cpp ///////////////////////////////////////////////////////////////
+//
+//  zzzzz      zzz   zzzzzzzzzzzzz   zzzz      zzzz      zzzzz  zzzzz
+//  zzzzzzz    zzz   zzzz              zzzz  zzzz      zzzz        zzzz
+//  zzz   zzz  zzz   zzzzzzzzz            zzzz        zzzz          zzzz
+//  zzz    zzzzzzz   zzzz              zzzz  zzzz      zzzz        zzzz
+//  zzz     zzzzzz   zzzzzzzzzzzzz   zzzz      zzzz      zzzzz  zzzzz
+//
+//  Author:      Marie Giacomel
+//  Date:        06/06/2025
+//  Description: Source file for the primitive customization window setup
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "PrimitiveWindow.hpp"
+
+namespace nexo::editor {
+
+    void PrimitiveWindow::shutdown()
+	{
+		// No need to delete anything since the destructor of the framebuffer will handle it
+	}
+
+}
