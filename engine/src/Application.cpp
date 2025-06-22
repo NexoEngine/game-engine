fc936b1758e153bff55a5a48c1f762e1a26b0a04
Thomas Parenteau <thomas.parenteau@epitech.eu>
2025-06-22 17:19:33 +0200

    fix(jolt): remove getEntitiesWith for querysystem and block update 60Hz

--- engine/src/Application.cpp
+++ engine/src/Application.cpp
@@ -306,6 +306,9 @@ namespace nexo {
 
         m_scriptingSystem->update();
 
+        constexpr float fixedTimestep = 1.0f / 60.0f;
+        static float physicsAccumulator = 0.0f;
+
         if (!m_isMinimized)
         {
          	renderContext.sceneRendered = static_cast<int>(sceneInfo.id);
@@ -328,7 +331,13 @@ namespace nexo {
 				// We have to unbind after the whole pipeline since multiple passes can use the same textures
 				// but we cant bind everything beforehand since a resize can be triggered and invalidate the whole state
                 renderer::NxRenderer3D::get().unbindTextures();
-        	    m_physicsSystem->update(m_currentTimestep);
+        	    physicsAccumulator += m_currentTimestep;
+
+        	    while (physicsAccumulator >= fixedTimestep)
+        	    {
+        	        m_physicsSystem->update(fixedTimestep);
+        	        physicsAccumulator -= fixedTimestep;
+        	    }
 			}
 			if (m_SceneManager.getScene(sceneInfo.id).isActive())
 			{
