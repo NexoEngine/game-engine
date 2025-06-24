2d9f34502371759f672781cb6c5e8f658e40d5f6
iMeaNz <mehdy.morvan@epitech.eu>
2025-06-24 17:03:19 +0900

    feat(mesh): add name component

---
+++ engine/src/components/Name.hpp
@@ -0,0 +1,22 @@
+//// Name.hpp ///////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Mehdy MORVAN
+//  Date:        24/06/2025
+//  Description: Header file for the name component
+//
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include <string>
+
+namespace nexo::components {
+    struct NameComponent {
+        std::string name;
+    };
+}
