fb0c547026d84e7d15003a566b610008abe3d1e9
Thyodas <guillaume.hein@epitech.eu>
2025-06-25 08:34:33 +0900

    refactor(scripting-ui): add UI component reflection api in C++ and C#
    Introduces a mechanism to reflect component fields for UI generation.
    This includes defining Field, FieldArray, and FieldType to describe component fields.
    The changes enable dynamic UI creation based on component structure.
    
    Removes unused NativeComponents.cs

---
+++ engine/src/scripting/managed/Components/Ui/FieldType.cs
@@ -0,0 +1,41 @@
+//// FieldType.cs /////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Guillaume HEIN
+//  Date:        24/06/2025
+//  Description: Source file for the FieldType enum in C#.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace Nexo.Components.Ui;
+
+public enum FieldType : UInt64
+{
+    // Special type, if blank, the field is not used 
+    Blank,
+    Section, // Used to create a section with title in the UI
+    
+    // Primitive types
+    Bool,
+    Int8,
+    Int16,
+    Int32,
+    Int64,
+    UInt8,
+    UInt16,
+    UInt32,
+    UInt64,
+    Float,
+    Double,
+    
+    // Widgets
+    Vector3,
+    Vector4,
+    
+    _Count // Count of the number of field types, used for validation
+}
