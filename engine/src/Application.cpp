1efb57268fe2318622b854ae8660825ea6db2423
Thomas Parenteau <thomas.parenteau@epitech.eu>
2025-06-26 18:12:24 +0200

    fix(jolt-3dPrimitive): rebase conflicts + erase one physics update

--- engine/src/Application.cpp
+++ engine/src/Application.cpp
@@ -319,7 +319,6 @@ namespace nexo {
                 m_transformHierarchySystem->update();
 				m_cameraContextSystem->update();
 				m_lightSystem->update();
-        	    m_physicsSystem->update(m_currentTimestep);
 				m_renderCommandSystem->update();
 				m_renderBillboardSystem->update();
 				for (auto &camera : renderContext.cameras)
@@ -327,6 +326,7 @@ namespace nexo {
 				// We have to unbind after the whole pipeline since multiple passes can use the same textures
 				// but we cant bind everything beforehand since a resize can be triggered and invalidate the whole state
                 renderer::NxRenderer3D::get().unbindTextures();
+        	    m_physicsSystem->update(m_worldState.time.deltaTime);
 			}
 			if (m_SceneManager.getScene(sceneInfo.id).isActive())
 			{
