90a97596329a73b77c64acaeaba391cfd3e2ed26
iMeaNz <mehdy.morvan@epitech.eu>
2025-07-09 22:41:32 +0200

    feat(front-material): add new material property

---
+++ editor/src/DocumentWindows/EntityProperties/MaterialProperty.hpp
@@ -0,0 +1,30 @@
+//// MaterialProperty.hpp ///////////////////////////////////////////////////////////////
+//
+//  zzzzz       zzz  zzzzzzzzzzzzz    zzzz      zzzz       zzzzzz  zzzzz
+//  zzzzzzz     zzz  zzzz                    zzzz       zzzz           zzzz
+//  zzz   zzz   zzz  zzzzzzzzzzzzz         zzzz        zzzz             zzz
+//  zzz    zzz  zzz  z                  zzzz  zzzz      zzzz           zzzz
+//  zzz         zzz  zzzzzzzzzzzzz    zzzz       zzz      zzzzzzz  zzzzz
+//
+//  Author:      Mehdy MORVAN
+//  Date:        25/06/2025
+//  Description: Header file for the material component display
+//
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "AEntityProperty.hpp"
+#include "DocumentWindows/InspectorWindow/InspectorWindow.hpp"
+#include "DocumentWindows/PopupManager.hpp"
+
+namespace nexo::editor {
+    class MaterialProperty final : public AEntityProperty {
+        public:
+            using AEntityProperty::AEntityProperty;
+
+            void show(ecs::Entity entity) override;
+        private:
+            void createMaterialPopup(const ecs::Entity entity);
+            PopupManager m_popupManager;
+    };
+}
