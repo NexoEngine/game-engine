0d3fbee8178882675a09ea0e8bce72dd59679d95
iMeaNz <mehdy.morvan@epitech.eu>
2025-07-10 22:29:23 +0200

    style(front-material): remove comment + fix overall code styling

--- editor/src/context/ThumbnailCache.hpp
+++ editor/src/context/ThumbnailCache.hpp
@@ -20,64 +20,43 @@
 
 namespace nexo::editor {
     class ThumbnailCache {
-    public:
-        // Singleton instance getter
-        static ThumbnailCache& getInstance();
+        public:
+            static ThumbnailCache& getInstance();
+            ThumbnailCache(const ThumbnailCache&) = delete;
+            ThumbnailCache& operator=(const ThumbnailCache&) = delete;
+            ThumbnailCache(ThumbnailCache&&) = delete;
+            ThumbnailCache& operator=(ThumbnailCache&&) = delete;
 
-        // Delete copy and move constructors/operators
-        ThumbnailCache(const ThumbnailCache&) = delete;
-        ThumbnailCache& operator=(const ThumbnailCache&) = delete;
-        ThumbnailCache(ThumbnailCache&&) = delete;
-        ThumbnailCache& operator=(ThumbnailCache&&) = delete;
+            unsigned int getThumbnail(const assets::GenericAssetRef& assetRef,
+                                const glm::vec2& size = {128, 128});
+            unsigned int getMaterialThumbnail(const assets::AssetRef<assets::Material>& materialRef,
+                                            const glm::vec2& size = {128, 128});
+            unsigned int updateMaterialThumbnail(const assets::AssetRef<assets::Material> &materialRef,
+                                                const glm::vec2 &size = {128, 128});
+            unsigned int getTextureThumbnail(const assets::AssetRef<assets::Texture>& textureRef,
+                                            const glm::vec2& size = {128, 128});
 
-        unsigned int getThumbnail(const assets::GenericAssetRef& assetRef,
-                               const glm::vec2& size = {128, 128});
+            void clearCache();
 
-        // Get thumbnail for material (creates if not exists)
-        unsigned int getMaterialThumbnail(const assets::AssetRef<assets::Material>& materialRef,
-                                         const glm::vec2& size = {128, 128});
+            void removeThumbnail(const boost::uuids::uuid& assetId);
+            bool hasThumbnail(const boost::uuids::uuid& assetId) const;
+        private:
+            ThumbnailCache() = default;
+            ~ThumbnailCache();
 
-        unsigned int updateMaterialThumbnail(const assets::AssetRef<assets::Material> &materialRef,
-                                            const glm::vec2 &size = {128, 128});
+            unsigned int createMaterialThumbnail(const assets::AssetRef<assets::Material>& materialRef,
+                                            const glm::vec2& size);
+            unsigned int createTextureThumbnail(const assets::AssetRef<assets::Texture>& textureRef,
+                                            const glm::vec2& size);
 
-        // Get thumbnail for texture (creates if not exists)
-        unsigned int getTextureThumbnail(const assets::AssetRef<assets::Texture>& textureRef,
-                                        const glm::vec2& size = {128, 128});
+            struct ThumbnailInfo {
+                std::shared_ptr<renderer::NxFramebuffer> framebuffer;
+                unsigned int textureId;
+                glm::vec2 size;
+                utils::ScenePreviewOut previewInfo;
+                bool isScenePreview; // true if using a scene preview, false if direct texture
+            };
 
-        // Clear all cached thumbnails
-        void clearCache();
-
-        // Remove a specific thumbnail
-        void removeThumbnail(const boost::uuids::uuid& assetId);
-
-        // Check if thumbnail exists
-        bool hasThumbnail(const boost::uuids::uuid& assetId) const;
-
-    private:
-        // Private constructor for singleton
-        ThumbnailCache() = default;
-        ~ThumbnailCache();
-
-        // Helper methods to create thumbnails
-        unsigned int createMaterialThumbnail(const assets::AssetRef<assets::Material>& materialRef,
-                                          const glm::vec2& size);
-
-        unsigned int createTextureThumbnail(const assets::AssetRef<assets::Texture>& textureRef,
-                                         const glm::vec2& size);
-
-        // Cache storage
-        struct ThumbnailInfo {
-            std::shared_ptr<renderer::NxFramebuffer> framebuffer;
-            unsigned int textureId;
-            glm::vec2 size;
-            utils::ScenePreviewOut previewInfo;
-            bool isScenePreview; // true if using a scene preview, false if direct texture
-        };
-
-        // Map of asset ID to thumbnail info
-        std::unordered_map<boost::uuids::uuid, ThumbnailInfo> m_thumbnailCache;
-
-        // Mutex for thread safety
-        mutable std::mutex m_cacheMutex;
+            std::unordered_map<boost::uuids::uuid, ThumbnailInfo> m_thumbnailCache;
     };
 }
