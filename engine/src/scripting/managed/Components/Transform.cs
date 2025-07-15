d1be5c6339a22b5d6c34292c70ecf9dd70db4604
Thyodas <guillaume.hein@epitech.eu>
2025-07-15 16:50:52 +0200

    feat(scripting): add localMatrix and worldMatrix to Transform, log localMatrix in NativeInterop

--- engine/src/scripting/managed/Components/Transform.cs
+++ engine/src/scripting/managed/Components/Transform.cs
@@ -24,6 +24,14 @@ namespace Nexo.Components
         public Vector3 pos;
         public Vector3 size;
         public Quaternion quat;
+        
+        public Matrix4x4 worldMatrix;
+        public Matrix4x4 localMatrix;
+
+        public Vector3 localCenter;
+
+        // Note: children is a C++ vector class, it cannot be directly represented in C#.
+        // TODO: Find a way to handle vectors and lists between C++ and C#.
     }
 
 }
