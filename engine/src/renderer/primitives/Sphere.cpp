eb4dc8148000378cd4bcaf2fa597532f658fe89e
Marie Giacomel <marie.giacomel@epitech.eu>
2025-07-16 16:18:34 +0200

    fix: directly add all sphere indices by default instead of adding them one by one

--- engine/src/renderer/primitives/Sphere.cpp
+++ engine/src/renderer/primitives/Sphere.cpp
@@ -69,69 +69,28 @@ namespace nexo::renderer
 
     static std::vector<unsigned int> generateSphereIndices()
     {
-        std::vector<unsigned int> indices{};
-        indices.reserve(60); // Reserve space for the 60 indices of the icosahedron
-
-        indices.push_back(2);
-        indices.push_back(1);
-        indices.push_back(0);
-        indices.push_back(1);
-        indices.push_back(2);
-        indices.push_back(3);
-        indices.push_back(5);
-        indices.push_back(4);
-        indices.push_back(3);
-        indices.push_back(4);
-        indices.push_back(8);
-        indices.push_back(3);
-        indices.push_back(7);
-        indices.push_back(6);
-        indices.push_back(0);
-        indices.push_back(6);
-        indices.push_back(9);
-        indices.push_back(0);
-        indices.push_back(11);
-        indices.push_back(10);
-        indices.push_back(4);
-        indices.push_back(10);
-        indices.push_back(11);
-        indices.push_back(6);
-        indices.push_back(9);
-        indices.push_back(5);
-        indices.push_back(2);
-        indices.push_back(5);
-        indices.push_back(9);
-        indices.push_back(11);
-        indices.push_back(8);
-        indices.push_back(7);
-        indices.push_back(1);
-        indices.push_back(7);
-        indices.push_back(8);
-        indices.push_back(10);
-        indices.push_back(2);
-        indices.push_back(5);
-        indices.push_back(3);
-        indices.push_back(8);
-        indices.push_back(1);
-        indices.push_back(3);
-        indices.push_back(9);
-        indices.push_back(2);
-        indices.push_back(0);
-        indices.push_back(1);
-        indices.push_back(7);
-        indices.push_back(0);
-        indices.push_back(11);
-        indices.push_back(9);
-        indices.push_back(6);
-        indices.push_back(7);
-        indices.push_back(10);
-        indices.push_back(6);
-        indices.push_back(5);
-        indices.push_back(11);
-        indices.push_back(4);
-        indices.push_back(10);
-        indices.push_back(8);
-        indices.push_back(4);
+        std::vector<unsigned int> indices = {
+            2, 1, 0,
+            1, 2, 3,
+            5, 4, 3,
+            4, 8, 3,
+            7, 6, 0,
+            6, 9, 0,
+            11, 10, 4,
+            10, 11, 6,
+            9, 5, 2,
+            5, 9, 11,
+            8, 7, 1,
+            7, 8, 10,
+            2, 5, 3,
+            8, 1, 3,
+            9, 2, 0,
+            1, 7, 0,
+            11, 9, 6,
+            7, 10, 6,
+            5, 11, 4,
+            10, 8, 4
+        };
 
         return indices;
     }
