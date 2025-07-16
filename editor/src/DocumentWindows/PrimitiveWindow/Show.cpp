bb0fb2efd9fadf28876b574cef743552673749e2
Marie Giacomel <marie.giacomel@epitech.eu>
2025-07-16 15:05:56 +0200

    fix: documentation and code style on different Show function for EditorScene, PrimitiveWindow and SceneTree

--- editor/src/DocumentWindows/PrimitiveWindow/Show.cpp
+++ editor/src/DocumentWindows/PrimitiveWindow/Show.cpp
@@ -15,44 +15,50 @@
 #include "PrimitiveWindow.hpp"
 #include "DocumentWindows/InspectorWindow/InspectorWindow.hpp"
 
-namespace nexo::editor {
+namespace nexo::editor
+{
+    void PrimitiveWindow::renderPrimitiveWindow() const
+    {
+        if (m_selectedPrimitive == CUBE)
+        {
+            ImGui::Text("Cube customization options");
+        }
+        else if (m_selectedPrimitive == SPHERE)
+        {
+            ImGui::Text("Sphere customization options");
+        }
+        else if (m_selectedPrimitive == CYLINDER)
+        {
+            ImGui::Text("Cylinder customization options");
+        }
+        else if (m_selectedPrimitive == PYRAMID)
+        {
+            ImGui::Text("Pyramid customization options");
+        }
+        else if (m_selectedPrimitive == TETRAHEDRON)
+        {
+            ImGui::Text("Tetrahedron customization options");
+        }
+    }
 
-	void PrimitiveWindow::renderPrimitiveWindow()
-	{
-		if (m_selectedPrimitive == CUBE) {
-			ImGui::Text("Cube customization options");
-		}
-		else if (m_selectedPrimitive == SPHERE) {
-			ImGui::Text("Sphere customization options");
-		}
-		else if (m_selectedPrimitive == CYLINDER) {
-			ImGui::Text("Cylinder customization options");
-		}
-		else if (m_selectedPrimitive == PYRAMID) {
-			ImGui::Text("Pyramid customization options");
-		}
-		else if (m_selectedPrimitive == TETRAHEDRON) {
-			ImGui::Text("Tetrahedron customization options");
-		}
-	}
+    void PrimitiveWindow::show()
+    {
+        const auto inspectorWindow = m_windowRegistry.getWindow<InspectorWindow>(NEXO_WND_USTRID_INSPECTOR).lock();
+        if (!inspectorWindow)
+            return;
+        if (inspectorWindow->getSubInspectorVisibility<PrimitiveWindow>())
+        {
+            ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoCollapse;
+            if (m_firstOpened)
+                window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
 
-	void PrimitiveWindow::show()
-	{
-		const auto inspectorWindow = m_windowRegistry.getWindow<InspectorWindow>(NEXO_WND_USTRID_INSPECTOR).lock();
-		if (!inspectorWindow)
-			return;
-		if (inspectorWindow->getSubInspectorVisibility<PrimitiveWindow>())
-		{
-			ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoCollapse;
-			if (m_firstOpened)
-				window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
-
-			if (ImGui::Begin("Primitive Window" NEXO_WND_USTRID_PRIMITIVE_WINDOW, &inspectorWindow->getSubInspectorVisibility<PrimitiveWindow>(), window_flags))
-			{
-				beginRender(NEXO_WND_USTRID_PRIMITIVE_WINDOW);
-				renderPrimitiveWindow();
-			}
-			ImGui::End();
-		}
-	}
+            if (ImGui::Begin("Primitive Window" NEXO_WND_USTRID_PRIMITIVE_WINDOW,
+                             &inspectorWindow->getSubInspectorVisibility<PrimitiveWindow>(), window_flags))
+            {
+                beginRender(NEXO_WND_USTRID_PRIMITIVE_WINDOW);
+                renderPrimitiveWindow();
+            }
+            ImGui::End();
+        }
+    }
 }
