f97e147abf75c5319c5fb0421037c60cc9b7ccea
iMeaNz <mehdy.morvan@epitech.eu>
2025-07-25 16:05:04 +0200

    refactor(render-passes): outline pass now retrieve the framebuffer from mask pass and draws onto the same render target as forward and grid without copying

--- engine/src/renderPasses/OutlinePass.cpp
+++ engine/src/renderPasses/OutlinePass.cpp
@@ -25,41 +25,29 @@
 #include <glad/glad.h>
 
 namespace nexo::renderer {
-    OutlinePass::OutlinePass(unsigned int width, unsigned int height) : RenderPass(Passes::OUTLINE, "Outline pass")
+    OutlinePass::OutlinePass() : RenderPass(Passes::OUTLINE, "Outline pass")
     {
-        renderer::NxFramebufferSpecs framebufferSpecs;
-        framebufferSpecs.attachments = {
-            renderer::NxFrameBufferTextureFormats::RGBA8,
-            renderer::NxFrameBufferTextureFormats::RED_INTEGER,
-            renderer::NxFrameBufferTextureFormats::Depth
-        };
-        framebufferSpecs.width = width;
-        framebufferSpecs.height = height;
-        m_output = renderer::NxFramebuffer::create(framebufferSpecs);
+
     }
 
     void OutlinePass::execute(RenderPipeline& pipeline)
     {
-        auto output = (m_isFinal) ? pipeline.getFinalRenderTarget() : m_output;
+        const std::shared_ptr<renderer::NxFramebuffer> renderTarget = pipeline.getRenderTarget();
         std::shared_ptr<NxFramebuffer> maskPass = nullptr;
-        std::shared_ptr<NxFramebuffer> prevPass = nullptr;
         for (const auto &prereq : prerequisites) {
             const auto &prereqPass = pipeline.getRenderPass(prereq);
             if (prereqPass->getId() == Passes::MASK) {
-                maskPass = pipeline.getOutput(prereqPass->getId());
-            } else {
-                prevPass = pipeline.getOutput(prereqPass->getId());
+                maskPass = prereqPass->getOutput();
             }
         }
-        if (!prevPass || !maskPass)
+        if (!renderTarget || !maskPass)
             return;
-        output->copy(prevPass);
-        output->bind();
+        renderTarget->bind();
 
         renderer::NxRenderCommand::setDepthTest(false);
         renderer::NxRenderCommand::setDepthMask(false);
         maskPass->bindAsTexture();
-        output->bindDepthAsTexture(1);
+        renderTarget->bindDepthAsTexture(1);
         maskPass->bindDepthAsTexture(2);
         const auto &drawCommands = pipeline.getDrawCommands();
         for (const auto &cmd : drawCommands) {
@@ -67,16 +55,8 @@ namespace nexo::renderer {
                 cmd.execute();
             }
         }
-        output->unbind();
+        renderTarget->unbind();
         renderer::NxRenderCommand::setDepthMask(true);
         renderer::NxRenderCommand::setDepthTest(true);
-        pipeline.setOutput(id, output);
-    }
-
-    void OutlinePass::resize(unsigned int width, unsigned int height)
-    {
-        if (!m_output)
-            return;
-        m_output->resize(width, height);
     }
 }
