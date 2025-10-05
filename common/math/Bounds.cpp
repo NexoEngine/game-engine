13865729b2126127eff84ba2038cfbab2fa0cfc5
iMeaNz <mehdy.morvan@epitech.eu>
2025-10-05 05:40:30 +0200

    feat(asset-aab): add utils func for aabb

---
+++ common/math/Bounds.cpp
@@ -0,0 +1,75 @@
+//// Bounds.cpp ///////////////////////////////////////////////////
+//
+// ⢀⢀⢀⣤⣤⣤⡀⢀⢀⢀⢀⢀⢀⢠⣤⡄⢀⢀⢀⢀⣠⣤⣤⣤⣤⣤⣤⣤⣤⣤⡀⢀⢀⢀⢠⣤⣄⢀⢀⢀⢀⢀⢀⢀⣤⣤⢀⢀⢀⢀⢀⢀⢀⢀⣀⣄⢀⢀⢠⣄⣀⢀⢀⢀⢀⢀⢀⢀
+// ⢀⢀⢀⣿⣿⣿⣷⡀⢀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡟⡛⡛⡛⡛⡛⡛⡛⢁⢀⢀⢀⢀⢻⣿⣦⢀⢀⢀⢀⢠⣾⡿⢃⢀⢀⢀⢀⢀⣠⣾⣿⢿⡟⢀⢀⡙⢿⢿⣿⣦⡀⢀⢀⢀⢀
+// ⢀⢀⢀⣿⣿⡛⣿⣷⡀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⡙⣿⡷⢀⢀⣰⣿⡟⢁⢀⢀⢀⢀⢀⣾⣿⡟⢁⢀⢀⢀⢀⢀⢀⢀⡙⢿⣿⡆⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⡈⢿⣷⡄⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⣇⣀⣀⣀⣀⣀⣀⣀⢀⢀⢀⢀⢀⢀⢀⡈⢀⢀⣼⣿⢏⢀⢀⢀⢀⢀⢀⣼⣿⡏⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⡘⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⡈⢿⣿⡄⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⣿⢿⢿⢿⢿⢿⢿⢿⢇⢀⢀⢀⢀⢀⢀⢀⢠⣾⣿⣧⡀⢀⢀⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⡈⢿⣿⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣰⣿⡟⡛⣿⣷⡄⢀⢀⢀⢀⢀⢿⣿⣇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⢀⡈⢿⢀⢀⢸⣿⡇⢀⢀⢀⢀⡛⡟⢁⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣼⣿⡟⢀⢀⡈⢿⣿⣄⢀⢀⢀⢀⡘⣿⣿⣄⢀⢀⢀⢀⢀⢀⢀⢀⢀⣼⣿⢏⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⢀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⢀⣀⣀⣀⣀⣀⣀⣀⣀⣀⡀⢀⢀⢀⣠⣾⡿⢃⢀⢀⢀⢀⢀⢻⣿⣧⡀⢀⢀⢀⡈⢻⣿⣷⣦⣄⢀⢀⣠⣤⣶⣿⡿⢋⢀⢀⢀⢀
+// ⢀⢀⢀⢿⢿⢀⢀⢀⢀⢀⢀⢀⢀⢸⢿⢃⢀⢀⢀⢀⢻⢿⢿⢿⢿⢿⢿⢿⢿⢿⢃⢀⢀⢀⢿⡟⢁⢀⢀⢀⢀⢀⢀⢀⡙⢿⡗⢀⢀⢀⢀⢀⡈⡉⡛⡛⢀⢀⢹⡛⢋⢁⢀⢀⢀⢀⢀⢀
+//
+//  Author:      Mehdy MORVAN
+//  Date:        05/10/2025
+//  Description: Source file for the bounds utils
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "Bounds.hpp"
+
+namespace nexo::math {
+    bool AABB::empty() const
+    {
+        return min.x > max.x || min.y > max.y || min.z > max.z;
+    }
+
+    AABB aabbUnion(const AABB& a, const AABB& b)
+    {
+        if (a.empty())
+            return b;
+        if (b.empty())
+            return a;
+
+        AABB o;
+        o.min = glm::min(a.min, b.min);
+        o.max = glm::max(a.max, b.max);
+        return o;
+    }
+
+    BSphere sphereFromAABB(const AABB& b)
+    {
+        BSphere s{};
+        if (b.empty())
+            return s;
+        s.c = 0.5f * (b.min + b.max);
+        const glm::vec3 e = 0.5f * (b.max - b.min);
+        s.r = glm::length(e);
+        return s;
+    }
+
+    AABB aabbTransform(const AABB& local, const glm::mat4& M)
+    {
+        if (local.empty())
+            return {};
+        const glm::vec3 lc = 0.5f * (local.min + local.max);
+        const glm::vec3 le = 0.5f * (local.max - local.min);
+
+        const glm::vec3 t = { M[3][0], M[3][1], M[3][2] };
+        // upper-left 3x3
+        const glm::mat3 L = glm::mat3(M);
+
+        // world center
+        const glm::vec3 wc = L * lc + t;
+        // world half-extents = |L| * le
+        const glm::mat3 A  = glm::mat3( glm::abs(L[0][0]), glm::abs(L[0][1]), glm::abs(L[0][2]),
+                                        glm::abs(L[1][0]), glm::abs(L[1][1]), glm::abs(L[1][2]),
+                                        glm::abs(L[2][0]), glm::abs(L[2][1]), glm::abs(L[2][2]) );
+        const glm::vec3 we  = A * le;
+
+        AABB out;
+        out.min = wc - we;
+        out.max = wc + we;
+        return out;
+    }
+}
