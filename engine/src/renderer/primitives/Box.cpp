62680f551633f2373c67da31e67e26a7160cd53a
iMeaNz <mehdy.morvan@epitech.eu>
2025-10-05 05:41:13 +0200

    feat(asset-aabb): add simple box primitive with only 8 vertices

---
+++ engine/src/renderer/primitives/Box.cpp
@@ -0,0 +1,64 @@
+//// Box.cpp //////////////////////////////////////////////////////////////////
+//
+// ⢀⢀⢀⣤⣤⣤⡀⢀⢀⢀⢀⢀⢀⢠⣤⡄⢀⢀⢀⢀⣠⣤⣤⣤⣤⣤⣤⣤⣤⣤⡀⢀⢀⢀⢠⣤⣄⢀⢀⢀⢀⢀⢀⢀⣤⣤⢀⢀⢀⢀⢀⢀⢀⢀⣀⣄⢀⢀⢠⣄⣀⢀⢀⢀⢀⢀⢀⢀
+// ⢀⢀⢀⣿⣿⣿⣷⡀⢀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡟⡛⡛⡛⡛⡛⡛⡛⢁⢀⢀⢀⢀⢻⣿⣦⢀⢀⢀⢀⢠⣾⡿⢃⢀⢀⢀⢀⢀⣠⣾⣿⢿⡟⢀⢀⡙⢿⢿⣿⣦⡀⢀⢀⢀⢀
+// ⢀⢀⢀⣿⣿⡛⣿⣷⡀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⡙⣿⡷⢀⢀⣰⣿⡟⢁⢀⢀⢀⢀⢀⣾⣿⡟⢁⢀⢀⢀⢀⢀⢀⢀⡙⢿⣿⡆⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⡈⢿⣷⡄⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⣇⣀⣀⣀⣀⣀⣀⣀⢀⢀⢀⢀⢀⢀⢀⡈⢀⢀⣼⣿⢏⢀⢀⢀⢀⢀⢀⣼⣿⡏⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⡘⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⡈⢿⣿⡄⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⣿⢿⢿⢿⢿⢿⢿⢿⢇⢀⢀⢀⢀⢀⢀⢀⢠⣾⣿⣧⡀⢀⢀⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⡈⢿⣿⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣰⣿⡟⡛⣿⣷⡄⢀⢀⢀⢀⢀⢿⣿⣇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⢀⡈⢿⢀⢀⢸⣿⡇⢀⢀⢀⢀⡛⡟⢁⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣼⣿⡟⢀⢀⡈⢿⣿⣄⢀⢀⢀⢀⡘⣿⣿⣄⢀⢀⢀⢀⢀⢀⢀⢀⢀⣼⣿⢏⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⢀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⢀⣀⣀⣀⣀⣀⣀⣀⣀⣀⡀⢀⢀⢀⣠⣾⡿⢃⢀⢀⢀⢀⢀⢻⣿⣧⡀⢀⢀⢀⡈⢻⣿⣷⣦⣄⢀⢀⣠⣤⣶⣿⡿⢋⢀⢀⢀⢀
+// ⢀⢀⢀⢿⢿⢀⢀⢀⢀⢀⢀⢀⢀⢸⢿⢃⢀⢀⢀⢀⢻⢿⢿⢿⢿⢿⢿⢿⢿⢿⢃⢀⢀⢀⢿⡟⢁⢀⢀⢀⢀⢀⢀⢀⡙⢿⡗⢀⢀⢀⢀⢀⡈⡉⡛⡛⢀⢀⢹⡛⢋⢁⢀⢀⢀⢀⢀⢀
+//
+//  Author:      Mehdy MORVAN
+//  Date:        05/10/2025
+//  Description: Source file for the box primitive (used for debugging AABB)
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "renderer/Renderer3D.hpp"
+
+namespace nexo::renderer {
+
+    struct NxBoxVertex {
+        glm::vec3 pos{};
+    };
+
+    static constexpr std::array<uint32_t, 24> kBoxEdgeIndices = {
+        0,1, 1,3, 3,2, 2,0,
+        4,5, 5,7, 7,6, 6,4,
+        0,4, 1,5, 2,6, 3,7
+    };
+
+    std::shared_ptr<NxVertexArray> NxRenderer3D::getBoxVAO()
+    {
+        static std::shared_ptr<NxVertexArray> s_boxVao;  // cached
+        if (s_boxVao) return s_boxVao;
+
+        static constexpr std::array<glm::vec3, 8> kCorners = {
+            glm::vec3{-1,-1,-1}, glm::vec3{+1,-1,-1}, glm::vec3{-1,-1,+1}, glm::vec3{+1,-1,+1},
+            glm::vec3{-1,+1,-1}, glm::vec3{+1,+1,-1}, glm::vec3{-1,+1,+1}, glm::vec3{+1,+1,+1}
+        };
+
+        auto vao = createVertexArray();
+
+        auto vbo = createVertexBuffer(static_cast<uint32_t>(kCorners.size() * sizeof(NxBoxVertex)));
+        const NxBufferLayout layout = { { NxShaderDataType::FLOAT3, "aPos" } };
+        vbo->setLayout(layout);
+
+        // Pack positions into NxBoxVertex
+        std::array<NxBoxVertex, kCorners.size()> verts{};
+        for (size_t i = 0; i < kCorners.size(); ++i) verts[i].pos = kCorners[i];
+
+        vbo->setData(verts.data(), static_cast<uint32_t>(verts.size() * sizeof(NxBoxVertex)));
+        vao->addVertexBuffer(vbo);
+
+        auto ebo = createIndexBuffer();
+        std::array<uint32_t, 24> indices = kBoxEdgeIndices;
+        ebo->setData(indices.data(), static_cast<unsigned int>(indices.size()));
+        vao->setIndexBuffer(ebo);
+
+        s_boxVao = std::move(vao);
+        return s_boxVao;
+    }
+}
