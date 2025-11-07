2a09ca23d0d345e9b4d219dde15368bf848d7dd3
Marie Giacomel <marie.giacomel@epitech.eu>
2025-11-07 11:35:32 +0100

    feat(add-component): add component to an entity in the UI

---
+++ editor/src/DocumentWindows/InspectorWindow/AddComponent.cpp
@@ -0,0 +1,63 @@
+//// AddComponent.cpp ///////////////////////////////////////////////////////////////
+//
+// ⢀⢀⢀⣤⣤⣤⡀⢀⢀⢀⢀⢀⢀⢠⣤⡄⢀⢀⢀⢀⣠⣤⣤⣤⣤⣤⣤⣤⣤⣤⡀⢀⢀⢀⢠⣤⣄⢀⢀⢀⢀⢀⢀⢀⣤⣤⢀⢀⢀⢀⢀⢀⢀⢀⣀⣄⢀⢀⢠⣄⣀⢀⢀⢀⢀⢀⢀⢀
+// ⢀⢀⢀⣿⣿⣿⣷⡀⢀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡟⡛⡛⡛⡛⡛⡛⡛⢁⢀⢀⢀⢀⢻⣿⣦⢀⢀⢀⢀⢠⣾⡿⢃⢀⢀⢀⢀⢀⣠⣾⣿⢿⡟⢀⢀⡙⢿⢿⣿⣦⡀⢀⢀⢀⢀
+// ⢀⢀⢀⣿⣿⡛⣿⣷⡀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⡙⣿⡷⢀⢀⣰⣿⡟⢁⢀⢀⢀⢀⢀⣾⣿⡟⢁⢀⢀⢀⢀⢀⢀⢀⡙⢿⣿⡆⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⡈⢿⣷⡄⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⣇⣀⣀⣀⣀⣀⣀⣀⢀⢀⢀⢀⢀⢀⢀⡈⢀⢀⣼⣿⢏⢀⢀⢀⢀⢀⢀⣼⣿⡏⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⡘⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⡈⢿⣿⡄⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⣿⢿⢿⢿⢿⢿⢿⢿⢇⢀⢀⢀⢀⢀⢀⢀⢠⣾⣿⣧⡀⢀⢀⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⡈⢿⣿⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣰⣿⡟⡛⣿⣷⡄⢀⢀⢀⢀⢀⢿⣿⣇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⢀⡈⢿⢀⢀⢸⣿⡇⢀⢀⢀⢀⡛⡟⢁⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣼⣿⡟⢀⢀⡈⢿⣿⣄⢀⢀⢀⢀⡘⣿⣿⣄⢀⢀⢀⢀⢀⢀⢀⢀⢀⣼⣿⢏⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⢀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⢀⣀⣀⣀⣀⣀⣀⣀⣀⣀⡀⢀⢀⢀⣠⣾⡿⢃⢀⢀⢀⢀⢀⢻⣿⣧⡀⢀⢀⢀⡈⢻⣿⣷⣦⣄⢀⢀⣠⣤⣶⣿⡿⢋⢀⢀⢀⢀
+// ⢀⢀⢀⢿⢿⢀⢀⢀⢀⢀⢀⢀⢀⢸⢿⢃⢀⢀⢀⢀⢻⢿⢿⢿⢿⢿⢿⢿⢿⢿⢃⢀⢀⢀⢿⡟⢁⢀⢀⢀⢀⢀⢀⢀⡙⢿⡗⢀⢀⢀⢀⢀⡈⡉⡛⡛⢀⢀⢹⡛⢋⢁⢀⢀⢀⢀⢀⢀
+//
+//  Author:      Marie GIACOMEL
+//  Date:        2025-10-30
+//  Description: Source file for the AddComponent functions
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+#include <ImNexo/Elements.hpp>
+
+#include "InspectorWindow.hpp"
+
+namespace nexo::editor {
+
+    void searchComponentBar()
+    {
+        // TODO: Implement search bar for components
+    }
+
+    void addComponentPopup(const ecs::Entity entity)
+    {
+        const auto& coord    = nexo::Application::m_coordinator;
+        const auto signature = coord->getSignature(entity);
+        ImGui::Text("Add component to %u", entity);
+        ImGui::Separator();
+        for (const auto& [componentType, description] : coord->getComponentDescriptions()) {
+            // Skip empty components or if the entity already has it
+            if (description->name.empty() || signature.test(componentType)) continue;
+            if (ImGui::Selectable(description->name.c_str())) {
+                LOG(NEXO_INFO, "Adding component {} to entity {}", description->name, entity);
+                coord->addComponentWithDefault(entity, componentType);
+            }
+        }
+        ImGui::Separator();
+        if (ImNexo::Button("Cancel", ImNexo::ButtonTypes::CANCEL)) {
+            PopupManager::closePopup();
+        }
+        PopupManager::endPopup();
+    }
+
+    void InspectorWindow::showAddComponentButton(const ecs::Entity entity)
+    {
+        if (ImGui::Button("Add Component")) {
+            ImGui::Text("add component to entity %u", entity);
+            m_popupManager.openPopup("Add Component Popup");
+        }
+
+        if (m_popupManager.showPopup("Add Component Popup")) {
+            addComponentPopup(entity);
+        }
+    }
+
+} // namespace nexo::editor
