666f9305f425000bb96661b56a48bb7e94f8ac17
iMeaNz <mehdy.morvan@epitech.eu>
2025-11-08 03:40:41 +0100

    feat(performance-maintenance): add gpu resources types def + constants

---
+++ engine/src/renderPasses/GPUResources.hpp
@@ -0,0 +1,98 @@
+//// GPUResources.hpp ///////////////////////////////////////////////////
+//
+// ⢀⢀⢀⣤⣤⣤⡀⢀⢀⢀⢀⢀⢀⢠⣤⡄⢀⢀⢀⢀⣠⣤⣤⣤⣤⣤⣤⣤⣤⣤⡀⢀⢀⢀⢠⣤⣄⢀⢀⢀⢀⢀⢀⢀⣤⣤⢀⢀⢀⢀⢀⢀⢀⢀⣀⣄⢀⢀⢠⣄⣀⢀⢀⢀⢀⢀⢀⢀
+// ⢀⢀⢀⣿⣿⣿⣷⡀⢀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡟⡛⡛⡛⡛⡛⡛⡛⢁⢀⢀⢀⢀⢻⣿⣦⢀⢀⢀⢀⢠⣾⡿⢃⢀⢀⢀⢀⢀⣠⣾⣿⢿⡟⢀⢀⡙⢿⢿⣿⣦⡀⢀⢀⢀⢀
+// ⢀⢀⢀⣿⣿⡛⣿⣷⡀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⡙⣿⡷⢀⢀⣰⣿⡟⢁⢀⢀⢀⢀⢀⣾⣿⡟⢁⢀⢀⢀⢀⢀⢀⢀⡙⢿⣿⡆⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⡈⢿⣷⡄⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⣇⣀⣀⣀⣀⣀⣀⣀⢀⢀⢀⢀⢀⢀⢀⡈⢀⢀⣼⣿⢏⢀⢀⢀⢀⢀⢀⣼⣿⡏⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⡘⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⡈⢿⣿⡄⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⣿⢿⢿⢿⢿⢿⢿⢿⢇⢀⢀⢀⢀⢀⢀⢀⢠⣾⣿⣧⡀⢀⢀⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⡈⢿⣿⢀⢀⢸⣿⡇⢀⢀⢀⢀⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣰⣿⡟⡛⣿⣷⡄⢀⢀⢀⢀⢀⢿⣿⣇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣿⣿⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⢀⡈⢿⢀⢀⢸⣿⡇⢀⢀⢀⢀⡛⡟⢁⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣼⣿⡟⢀⢀⡈⢿⣿⣄⢀⢀⢀⢀⡘⣿⣿⣄⢀⢀⢀⢀⢀⢀⢀⢀⢀⣼⣿⢏⢀⢀⢀
+// ⢀⢀⢀⣿⣿⢀⢀⢀⢀⢀⢀⢀⢀⢸⣿⡇⢀⢀⢀⢀⢀⣀⣀⣀⣀⣀⣀⣀⣀⣀⡀⢀⢀⢀⣠⣾⡿⢃⢀⢀⢀⢀⢀⢻⣿⣧⡀⢀⢀⢀⡈⢻⣿⣷⣦⣄⢀⢀⣠⣤⣶⣿⡿⢋⢀⢀⢀⢀
+// ⢀⢀⢀⢿⢿⢀⢀⢀⢀⢀⢀⢀⢀⢸⢿⢃⢀⢀⢀⢀⢻⢿⢿⢿⢿⢿⢿⢿⢿⢿⢃⢀⢀⢀⢿⡟⢁⢀⢀⢀⢀⢀⢀⢀⡙⢿⡗⢀⢀⢀⢀⢀⡈⡉⡛⡛⢀⢀⢹⡛⢋⢁⢀⢀⢀⢀⢀⢀
+//
+//  Author:      Mehdy MORVAN
+//  Date:        07/11/2025
+//  Description: Header file that contains types and constants for gpu resources
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <glm/glm.hpp>
+#include <string>
+#include <unordered_map>
+
+namespace nexo::renderer {
+    #define PER_VIEW_UBO "PerView"
+
+    struct GpuPerView {
+        glm::mat4 uViewProjection;
+        glm::vec3 uCamPos;
+        float _pad0; // to match std140 (vec3 -> 16 bytes)
+    };
+
+    #define LIGHT_UBO "LightBlock"
+
+    constexpr int MAX_POINT_LIGHTS = 10;
+    constexpr int MAX_SPOT_LIGHTS  = 10;
+
+    struct GpuDirectionalLight {
+        glm::vec3 direction;
+        float _pad0;         // matches GLSL
+        glm::vec4 color;
+    };
+
+    struct GpuPointLight {
+        glm::vec3 position;
+        float _pad1;
+        glm::vec4 color;
+        float constant;
+        float linear;
+        float quadratic;
+        float _pad2;         // pad to multiple of 16 bytes
+    };
+
+    struct GpuSpotLight {
+        glm::vec3 position;
+        float _pad3;
+
+        glm::vec3 direction;
+        float _pad4;
+
+        glm::vec4 color;
+
+        float cutOff;
+        float outerCutoff;
+        float constant;
+        float linear;
+
+        float quadratic;
+        float _pad5;
+        float _pad6;
+        float _pad7;
+    };
+
+    struct GpuLightBlock {
+        glm::vec3 ambientLight;
+        float _padAmbient;
+
+        GpuDirectionalLight dirLight;
+
+        GpuPointLight pointLights[MAX_POINT_LIGHTS];
+        GpuSpotLight  spotLights[MAX_SPOT_LIGHTS];
+
+        int numPointLights;
+        int numSpotLights;
+        int _padCount0;
+        int _padCount1;
+    };
+
+    #define NB_RESERVED "NB_RESERVED"
+
+    inline const std::unordered_map<std::string, unsigned int> RESERVED_BINDING_POINTS = {
+        {PER_VIEW_UBO, 1},
+        {LIGHT_UBO, 2},
+        {NB_RESERVED, 3}
+    };
+
+}
