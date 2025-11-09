04ed87eeaa23f5e31324fcdda412820ced8bbae1
iMeaNz <mehdy.morvan@epitech.eu>
2025-11-09 17:01:10 +0100

    feat(pbr): textures now supports pbr images and maps

--- engine/src/renderer/Texture.hpp
+++ engine/src/renderer/Texture.hpp
@@ -26,6 +26,18 @@
 
 namespace nexo::renderer {
 
+    enum class TextureType {
+        ALBEDO,      // sRGB, linear filtering, mipmaps
+        NORMAL,      // Linear, linear filtering, mipmaps
+        METALLIC,    // Linear, linear filtering, mipmaps
+        ROUGHNESS,   // Linear, linear filtering, mipmaps
+        AO,          // Linear, linear filtering, mipmaps
+        EMISSIVE,    // sRGB, linear filtering, mipmaps
+        HEIGHT,       // Linear, linear filtering, mipmaps
+        COMBINED_MR,
+        COMBINED_ORM,
+    };
+
     /**
      * @class NxTexture
      * @brief Abstract base class for representing textures in a rendering system.
