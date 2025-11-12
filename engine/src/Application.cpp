cf7110972e2352707cfc99b5f5d424a756e71082
Marie Giacomel <marie.giacomel@epitech.eu>
2025-11-12 12:44:39 +0100

    feat(physics): video system does not need play to update

--- engine/src/Application.cpp
+++ engine/src/Application.cpp
@@ -103,10 +103,10 @@ namespace nexo {
                 // We have to unbind after the whole pipeline since multiple passes can use the same textures
                 // but we cant bind everything beforehand since a resize can be triggered and invalidate the whole state
                 renderer::NxRenderer3D::get().unbindTextures();
+                m_renderVideoSystem->update();
 
                 if (isInPlayMode()) {
                     m_physicsSystem->update();
-                    m_renderVideoSystem->update();
                 }
             }
             if (m_SceneManager.getScene(sceneInfo.id).isActive()) {
